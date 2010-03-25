6e84a7007b47c0e32a4d5fe6bcd1946dfe5b763f
Jacob Hammack <jacob.hammack@hammackj.com>
2010-03-24 23:20:52 -0500

    Changed the way that the parser loops, going to change it a lot more very soon and add a command line parser

--- nessus_parser.rb
+++ nessus_parser.rb
@@ -10,21 +10,28 @@ require 'nessus_db'
 require 'nessus_db_model'
 require 'nessus_listener'
 
+#add debug mode for output
+
 #pass each .nessus file to be pasrsed into the database on the command line
 #each will be parsed into the database as seperate policy/reports
-begin
-  ARGV.each { |arg| 
-    puts "NessusDB Parser v1.0\nJacob Hammack\nhttp://hammackj.com\n\n"
-  	puts "[*] Parsing #{arg}..."
-  	tstart = Time.new
-  	parser = LibXML::XML::SaxParser.file arg
-  	parser.callbacks = NessusSaxListener.new
-  	parser.parse
-  	printf "[*] Finished parsing %s. Parse took %.02f seconds\n", arg, Time.now - tstart
-  }
-rescue Interrupt => i
-	puts "[!] Parse cancelled!"
-rescue => e
-	puts "An error occurred: #{e}"
-	puts "Backtrace: #{e.backtrace.join("\n")}"
-end  
+
+puts "NessusDB Parser v1.0\nJacob Hammack\nhttp://hammackj.com\n\n"
+
+ARGV.each { |arg| 
+  begin
+    	puts "[*] Parsing #{arg}..."
+    	tstart = Time.new
+    	parser = LibXML::XML::SaxParser.file arg
+    	parser.callbacks = NessusSaxListener.new
+    	parser.parse
+    	printf "[*] Finished parsing %s. Parse took %.02f seconds\n", arg, Time.now - tstart
+
+  rescue Interrupt => i
+  	puts "[!] Parse cancelled!"
+  rescue => e
+    puts "[!] Error: #{arg}"
+  	#puts "An error occurred: #{e}"
+  	#puts "Backtrace: #{e.backtrace.join("\n")}"
+    next
+  end  
+}
