a9016d4b91adcfb5b8667d4e5347bc582cc0aae0
Jacob Hammack <jacob.hammack@hammackj.com>
2010-09-21 00:27:05 -0500

    Added scan date to the template

--- lib/nessusdb/templates/finding_statistics.rb
+++ lib/nessusdb/templates/finding_statistics.rb
@@ -1,4 +1,11 @@
-text "Finding Statistics"
+font_size(18) {
+	text "Finding Statistics"
+	text "\n"
+}
+
+text "Scan Date:", :style => :bold
+text "#{findings.scan_date}"
+text "\n"
 
 table([["Number of hosts","Number of risks","Critical Risks", "High Risks", "Medium Risks", "Low Risks"], 
 [findings.number_of_hosts, findings.number_of_risks, findings.number_of_critical, findings.number_of_high, findings.number_of_medium, findings.number_of_low]], 
