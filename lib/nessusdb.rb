6e6c71ef0e5d5f6ea40344cb20adf8c254005cf2
Jacob Hammack <jacob.hammack@hammackj.com>
2010-10-04 16:54:08 -0500

    Cleaned up the imports and removed some old code.

--- lib/nessusdb.rb
+++ lib/nessusdb.rb
@@ -12,25 +12,9 @@ require 'gruff'
 require 'prawn'
 require 'prawn/layout' 
 
-#require 'erb'
-
 require 'nessusdb/models'
 require 'nessusdb/listener'
 require 'nessusdb/findings'
 require 'nessusdb/schema'
 require 'nessusdb/templater'
 require 'nessusdb/prawn_templater'
-
-
-# config/initializers/pdfkit.rb
-#PDFKit.configure do |config|
-#	if File.exists? "/opt/local/bin/wkhtmltopdf"
-#  	config.wkhtmltopdf = '/opt/local/bin/wkhtmltopdf'
-#	else
-#		config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
-#	end
-#  config.default_options = {
-#    :page_size => 'letter',
-#    :print_media_type => true
-#  }
-#end
