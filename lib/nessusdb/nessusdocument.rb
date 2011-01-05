6dc3fddd585e957490c7fd7b5fc2676932054500
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-05 16:23:55 -0600

    Added a Object to represent the nessus xml in memory

---
+++ lib/nessusdb/nessusdocument.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+module NessusDB
+	
+	# A Object to represet the Nessus xml file in memory
+	#
+	# @author Jacob Hammack
+	class NessusDocument
+		
+		# Creates a instance of the NessusDocument class
+		#
+		def initialize document
+			@document = document
+		end
+		
+		#
+		#
+		def validate
+			@parser = LibXML::XML::Parser.file @document
+			doc = @parser.parse
+			
+
+		end
+				
+		#
+		#
+		def parse
+			@parser = LibXML::XML::SaxParser.file @document
+    	@parser.callbacks = NessusSaxListener.new
+    	@parser.parse
+		end
+		
+	end
+end
