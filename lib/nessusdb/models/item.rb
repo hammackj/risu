b9b255fdfb445991fbfea8657313377761adcc70
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-12 17:38:32 -0600

    Added dynamic named scopes for all the risk variables. Previously were in findings

--- lib/nessusdb/models/item.rb
+++ lib/nessusdb/models/item.rb
@@ -1,3 +1,5 @@
+
+
 module NessusDB
 	module Modules
 		# Item Model
@@ -6,6 +8,43 @@ module NessusDB
 		class Item < ActiveRecord::Base
 		  belongs_to :hosts
 		  has_many :plugins
+		
+			class << self
+
+				#
+				#@number_of_risks = Item.find(:all, :conditions => ["severity IN (0,1,2,3,4) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
+				#
+				def risks
+					where(:severity => [0,1,2,3,4])
+				end
+			
+				#
+				# @number_of_critical = Item.find(:all, :conditions => ["severity IN (3) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
+				#
+				def critical_risks
+					where(:severity => 3)
+				end
+			
+				#
+				#@number_of_high = Item.find(:all, :conditions => ["severity IN (2) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
+				#
+				def high_risks
+					where(:severity => 2)
+				end
+				
+				#
+				#@number_of_medium = Item.find(:all, :conditions => ["severity IN (1) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
+				#
+				def medium_risks
+					where(:severity => 1)
+				end
+				
+				#
+				#@number_of_low = Item.find(:all, :conditions => ["severity IN (0) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
+				#
+				def low_risks
+					where(:severity => 0)
+				end
 		end
 	end
 end
