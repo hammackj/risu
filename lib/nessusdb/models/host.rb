51a570273483e5c8afff77a46d7ee0e6786d4c66
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-13 16:30:35 -0600

    Added dynamic named scopes to replace the findings class

--- lib/nessusdb/models/host.rb
+++ lib/nessusdb/models/host.rb
@@ -4,7 +4,7 @@ module NessusDB
 	module Modules
 		# Host Model
 		#
-		# @author Jacob Hammack
+		# @author Jacob Hammack <jacob.hammack@hammackj.com>
 		class Host < ActiveRecord::Base
 			belongs_to :Reports
 			has_many :Items
@@ -21,7 +21,44 @@ module NessusDB
 						count
 					end
 				end
-			
+				
+				
+				#
+				# @todo test for nil ip
+				# 
+				#
+				def sorted
+					hosts = Host.where("ip is not NULL").order("ip").all
+					
+					#The name is usally the ip also so if the ip is nil just use the name
+					hosts.each do |host|
+						if host.ip == nil
+							host.ip = host.name
+						end
+					end
+
+					#Sort the ips in natural order.
+					hosts.sort! { |a, b|
+						a.ip.gsub(".", "").to_i <=> b.ip.gsub(".", "").to_i
+					}
+
+					return hosts
+				end
+				
+				#
+				#@windows_operating_systems = Host.find(:all, :conditions => ["os like '%%Windows%%' AND id != #{@blacklist_host_id}"], :group => :os).map(&:os)
+				#
+				def os_windows
+					where("os like '%%Windows%%'")
+				end
+				
+				#
+				# @other_operating_systems = Host.find(:all, :conditions => ["os not like '%%Windows%%' AND id != #{@blacklist_host_id}"], :group => :os).map(&:os)
+				#
+				def os_other
+					where("os not like '%%Windows%%''")
+				end		
+			end
 		end
 	end
 end
