2ffe9e4f7bb38c0b0b046cdec80c605eacca6519
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-13 16:31:16 -0600

    Added dynamic named scoped methods to the model, replacing the findings class

--- lib/nessusdb/models/item.rb
+++ lib/nessusdb/models/item.rb
@@ -1,4 +1,4 @@
-
+# encoding: utf-8
 
 module NessusDB
 	module Modules
@@ -53,17 +53,34 @@ module NessusDB
 					where(:severity => 3).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
 				end
 				
+				#
+				#
+				#
 				def high_risks_unique
 					where(:severity => 2).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
 				end
 				
+				#
+				#
+				#
 				def medium_risks_unique
 					where(:severity => 1).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
 				end
 				
+				#
+				#
+				#
 				def low_risks_unique
 					where(:severity => 0).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
 				end
+				
+				#
+				#@findings_by_service = Item.find_by_sql("SELECT svc_name, count(*) as c FROM items where svc_name != 'unknown' and svc_name != 'general' group by svc_name order by c desc limit 10").map(&:svc_name)#Item.find(:all, :group => :svc_name).map(&:svc_name)
+				#
+				def risks_by_service
+					
+				end
+			end
 		end
 	end
 end
