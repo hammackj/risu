6c204a4af17058f7a562a15138ef907b6833bc34
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-12 22:41:54 -0600

    Added more named scopes to remove more from the findings class

--- lib/nessusdb/models/item.rb
+++ lib/nessusdb/models/item.rb
@@ -45,6 +45,25 @@ module NessusDB
 				def low_risks
 					where(:severity => 0)
 				end
+				
+				#
+				#@critical_findings_unique = Item.find(:all, :conditions => ["severity = 3 AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"], :joins => "INNER JOIN plugins ON items.plugin_id = plugins.id", :order => 'plugins.cvss_base_score', :group => :plugin_id)
+				#
+				def critical_risks_unique
+					where(:severity => 3).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
+				end
+				
+				def high_risks_unique
+					where(:severity => 2).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
+				end
+				
+				def medium_risks_unique
+					where(:severity => 1).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
+				end
+				
+				def low_risks_unique
+					where(:severity => 0).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
+				end
 		end
 	end
 end
