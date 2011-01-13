d28e14db5bd4c5fdf1e98ca11f59c5cb16b5230b
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-13 16:31:52 -0600

    added a report_date method

--- lib/nessusdb/models/report.rb
+++ lib/nessusdb/models/report.rb
@@ -1,11 +1,23 @@
+# encoding: utf-8
+
 module NessusDB
 	module Modules
 		# Report Model
 		#
-		# @author Jacob Hammack
+		# @author Jacob Hammack <jacob.hammack@hammackj.com>
 		class Report < ActiveRecord::Base
 		  has_many :Hosts
 		  belongs_to :policies
+		
+			class << self
+				
+				#
+				#@scan_date = Host.where("start is not null").first[:start].to_s
+				#
+				def report_date
+					Host.where("start is not null").first[:start].to_s
+				end
+			end
 		end
 	end
 end
