c189d10cd444b3590773cd6ff636edda5292ae8f
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-16 21:37:36 -0600

    Added valid? to the NessusDocument class

--- lib/nessusdb/nessusdocument.rb
+++ lib/nessusdb/nessusdocument.rb
@@ -4,7 +4,7 @@ module NessusDB
 	
 	# A Object to represet the Nessus xml file in memory
 	#
-	# @author Jacob Hammack
+	# @author Jacob Hammack <jacob.hammack@hammackj.com>
 	class NessusDocument
 		
 		# Creates a instance of the NessusDocument class
@@ -13,17 +13,27 @@ module NessusDB
 			@document = document
 		end
 		
+		# Checks the validness of a NessusDocument
 		#
-		#
-		def validate
+		# @return [Boolean] True if valid, False if invalid
+		def valid?
 			@parser = LibXML::XML::Parser.file @document
 			doc = @parser.parse
 			
-			puts doc.inspect
-
+			if doc == nil
+				return false
+			end
+			
+			node = doc.find("//NessusClientData_v2")
+			
+			if node.first.content != nil
+				return true
+			else
+				return false
+			end
 		end
 				
-		#
+		# Invokes the SAX parser on the XML document
 		#
 		def parse
 			@parser = LibXML::XML::SaxParser.file @document
