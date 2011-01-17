d31a25f0e151e38ccc4c284803eacebc6c9aae72
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-16 22:00:52 -0600

    trying to solve this include thing

--- lib/nessusdb.rb
+++ lib/nessusdb.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
-
 module NessusDB
 	APP_NAME = "nessusdb"
 	VERSION = "1.2"
@@ -20,3 +19,15 @@ require 'nessusdb/findings'
 require 'nessusdb/schema'
 require 'nessusdb/prawn_templater'
 require 'nessusdb/nessusdocument'
+
+require 'nessusdb/models/host'
+require 'nessusdb/models/familyselection'
+require 'nessusdb/models/item'
+require 'nessusdb/models/individualpluginselection'
+require 'nessusdb/models/plugin'
+require 'nessusdb/models/pluginspreference'
+require 'nessusdb/models/policy'
+require 'nessusdb/models/report'
+require 'nessusdb/models/reference'
+require 'nessusdb/models/serverpreference'
+require 'nessusdb/models/version'
