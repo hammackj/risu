faefe99ceb21e537b83e3a08b5ff2fe818288d68
Jacob Hammack <jacob.hammack@hammackj.com>
2011-02-06 13:05:37 -0600

    Added named scope functions for Item

--- lib/nessusdb/models/item.rb
+++ lib/nessusdb/models/item.rb
@@ -50,35 +50,49 @@ module NessusDB
 				#@critical_findings_unique = Item.find(:all, :conditions => ["severity = 3 AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"], :joins => "INNER JOIN plugins ON items.plugin_id = plugins.id", :order => 'plugins.cvss_base_score', :group => :plugin_id)
 				#
 				def critical_risks_unique
-					where(:severity => 3).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
+					where(:severity => 3).joins("INNER JOIN plugins ON items.plugin_id = plugins.id").order(:cvss_base_score).group(:plugin_id)
 				end
 				
 				#
 				#
 				#
 				def high_risks_unique
-					where(:severity => 2).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
+					where(:severity => 2).joins(:plugins).order(:cvss_base_score).group(:plugin_id)
 				end
 				
 				#
 				#
 				#
 				def medium_risks_unique
-					where(:severity => 1).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
+					where(:severity => 1).joins(:plugins).order(:cvss_base_score).group(:plugin_id)
 				end
 				
 				#
 				#
 				#
 				def low_risks_unique
-					where(:severity => 0).includes(:plugins).order(:cvss_base_score).group(:plugin_id)
+					where(:severity => 0).joins(:plugins).order(:cvss_base_score).group(:plugin_id)
 				end
 				
 				#
 				#@findings_by_service = Item.find_by_sql("SELECT svc_name, count(*) as c FROM items where svc_name != 'unknown' and svc_name != 'general' group by svc_name order by c desc limit 10").map(&:svc_name)#Item.find(:all, :group => :svc_name).map(&:svc_name)
 				#
 				def risks_by_service
-					
+					select("items.*").select("count(*) as count_all").where("svc_name != 'unknown' and svc_name != 'general'").group(:svc_name).order("count_all DESC")
+				end
+				
+				#
+				##@top_plugins = Item.find_by_sql("SELECT *, count(plugin_id) FROM items WHERE plugin_id NOT IN (1) AND severity in (3) GROUP BY plugin_id ORDER BY count(plugin_id) DESC LIMIT 5").map(&:plugin_id)
+				#
+				def risks_by_plugin
+					select("items.*").select("count() as count_all").where("plugin_id != 1").where(:severity => 3).group(:plugin_id).order("count_all DESC")
+				end
+				
+				#
+				##@top_vuln_hosts = Item.find_by_sql("SELECT host_id, count(host_id) FROM items WHERE plugin_id != 1 AND plugin_id NOT IN (#{@blacklist_plugins}) AND severity IN (3,2) GROUP BY host_id ORDER BY count(host_id) DESC LIMIT 10").map(&:host_id)
+				#
+				def risks_by_host
+					select("items.*").select("count(*) as count_all").where("plugin_id != 1").where(:severity => [3,2]).group(:host_id).order("count_all DESC")
 				end
 			end
 		end
