347dcba7c441ad85f684f888133c28b441c01b75
Jacob Hammack <jacob.hammack@hammackj.com>
2011-02-14 16:28:35 -0600

    Added comments for all the methods

--- lib/nessusdb/models/item.rb
+++ lib/nessusdb/models/item.rb
@@ -11,88 +11,88 @@ module NessusDB
 		
 			class << self
 
+				# Queries for all risks in the database
 				#
-				#@number_of_risks = Item.find(:all, :conditions => ["severity IN (0,1,2,3,4) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def risks
-					where(:severity => [0,1,2,3,4])
+					where(:severity => [0,1,2,3])
 				end
 			
-				#
-				# @number_of_critical = Item.find(:all, :conditions => ["severity IN (3) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
-				#
+				# Queries for all the critical risks in the database
+				# 
+				# @return [ActiveRecord::Relation] with the query results
 				def critical_risks
 					where(:severity => 3)
 				end
 			
+				# Queries for all the high risks in the database
 				#
-				#@number_of_high = Item.find(:all, :conditions => ["severity IN (2) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
-				#
+				# @return [ActiveRecord::Relation] with the query results 
 				def high_risks
 					where(:severity => 2)
 				end
 				
+				# Queries for all the medium risks in the database
 				#
-				#@number_of_medium = Item.find(:all, :conditions => ["severity IN (1) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def medium_risks
 					where(:severity => 1)
 				end
 				
+				# Queries for all the low risks in the database
 				#
-				#@number_of_low = Item.find(:all, :conditions => ["severity IN (0) AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"]).count
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def low_risks
 					where(:severity => 0)
 				end
 				
+				# Queries for all the unique critical risks in the database
 				#
-				#@critical_findings_unique = Item.find(:all, :conditions => ["severity = 3 AND plugin_id NOT IN (#{@blacklist_plugins}) AND host_id != #{@blacklist_host_id}"], :joins => "INNER JOIN plugins ON items.plugin_id = plugins.id", :order => 'plugins.cvss_base_score', :group => :plugin_id)
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def critical_risks_unique
 					where(:severity => 3).joins(:plugin).order("plugins.cvss_base_score").group(:plugin_id)
 				end
 				
+				# Queries for all the unique high risks in the database
 				#
-				#
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def high_risks_unique
 					where(:severity => 2).joins(:plugin).order(:cvss_base_score).group(:plugin_id)
 				end
 				
+				# Queries for all the unique medium risks in the database
 				#
-				#
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def medium_risks_unique
 					where(:severity => 1).joins(:plugin).order(:cvss_base_score).group(:plugin_id)
 				end
 				
+				# Queries for all the unique low risks in the database
 				#
-				#
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def low_risks_unique
 					where(:severity => 0).joins(:plugin).order(:cvss_base_score).group(:plugin_id)
 				end
 				
+				# Queries for all the risks grouped by service type, used for the Vulnerbilities by Service graph
 				#
-				#@findings_by_service = Item.find_by_sql("SELECT svc_name, count(*) as c FROM items where svc_name != 'unknown' and svc_name != 'general' group by svc_name order by c desc limit 10").map(&:svc_name)#Item.find(:all, :group => :svc_name).map(&:svc_name)
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def risks_by_service(limit=10)
-					select("items.*").select("count(*) as count_all").where("svc_name != 'unknown' and svc_name != 'general'").group(:svc_name).order("count_all DESC").limit(limit)#.all
+					select("items.*").select("count(*) as count_all").where("svc_name != 'unknown' and svc_name != 'general'").group(:svc_name).order("count_all DESC").limit(limit)
 				end
 				
+				# Queries for all the risks by plugin
 				#
-				##@top_plugins = Item.find_by_sql("SELECT *, count(plugin_id) FROM items WHERE plugin_id NOT IN (1) AND severity in (3) GROUP BY plugin_id ORDER BY count(plugin_id) DESC LIMIT 5").map(&:plugin_id)
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def risks_by_plugin(limit=10)
-					select("items.*").select("count() as count_all").joins(:plugin).where("plugin_id != 1").where(:severity => 3).group(:plugin_id).order("count_all DESC").limit(limit)#.all
+					select("items.*").select("count() as count_all").joins(:plugin).where("plugin_id != 1").where(:severity => 3).group(:plugin_id).order("count_all DESC").limit(limit)
 				end
 				
+				# Queries for all the risks by host
 				#
-				##@top_vuln_hosts = Item.find_by_sql("SELECT host_id, count(host_id) FROM items WHERE plugin_id != 1 AND plugin_id NOT IN (#{@blacklist_plugins}) AND severity IN (3,2) GROUP BY host_id ORDER BY count(host_id) DESC LIMIT 10").map(&:host_id)
-				#
+				# @return [ActiveRecord::Relation] with the query results
 				def risks_by_host(limit=10)
-					select("items.*").select("count(*) as count_all").joins(:host).where("plugin_id != 1").where(:severity => [3,2]).group(:host_id).order("count_all DESC").limit(limit)#.all
+					select("items.*").select("count(*) as count_all").joins(:host).where("plugin_id != 1").where(:severity => [3,2]).group(:host_id).order("count_all DESC").limit(limit)
 				end
 				
 				# Generates a Graph of all the risks by service
