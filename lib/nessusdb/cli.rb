abf535be13695c1c1d50e92c8e655c2646925722
Jacob Hammack <jacob.hammack@hammackj.com>
2011-02-15 20:27:01 -0600

    Moving the command line stuff into a library

---
+++ lib/nessusdb/cli.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+module NessusDB
+	module CLI
+	end
+end
---
+++ lib/nessusdb/cli/application.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+module NessusDB
+	module CLI
+		
+		#
+		#
+		class Application
+			
+			#
+			#
+			def initialize
+				
+			end
+			
+			#
+			#
+			def run(args)
+			end
+			
+		end
+	end
+end
