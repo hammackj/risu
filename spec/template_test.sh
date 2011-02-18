31b8b7c048c56f6d3eb7fd78d5d0e8afc7a13284
Jacob Hammack <jacob.hammack@hammackj.com>
2011-02-17 20:29:10 -0600

    Fixed the path to the test nessusdb binary

--- spec/template_test.sh
+++ spec/template_test.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
 echo "assets" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/assets.rb -o assets.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/assets.rb -o assets.pdf &&
 
 echo "stats" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/finding_statistics.rb -o stats.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/finding_statistics.rb -o stats.pdf &&
 
 echo "graphs" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/graphs.rb -o graphs.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/graphs.rb -o graphs.pdf &&
 
 echo "pci" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/pci_compliance.rb -o pci.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/pci_compliance.rb -o pci.pdf &&
 
 echo "tech" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/technical_findings.rb -o tech.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/technical_findings.rb -o tech.pdf &&
 
 echo "exec_summary" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/exec_summary.rb -o exec.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/exec_summary.rb -o exec.pdf &&
 
 echo "findings_summary" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/findings_summary.rb -o findings_summary.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/findings_summary.rb -o findings_summary.pdf &&
 
 echo "host_summary" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/host_summary.rb -o host_summary.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/host_summary.rb -o host_summary.pdf &&
 
 echo "ms_patch_summary" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/ms_patch_summary.rb -o patches.pdf &&
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/ms_patch_summary.rb -o patches.pdf &&
 
 echo "ms_update_summary" &&
-./nessusdb --author "Jacob" -t ../lib/nessusdb/templates/ms_updates_summary.rb -o updates.pdf
+../bin/nessusdb --author "Jacob" -t ../lib/nessusdb/templates/ms_updates_summary.rb -o updates.pdf
