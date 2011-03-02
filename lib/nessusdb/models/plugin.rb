7f8832e9c5d22899a541090aff96873688b256a0
Jacob Hammack <jacob.hammack@hammackj.com>
2011-03-02 16:37:11 -0600

    Active Record Relation Issue

--- lib/nessusdb/models/plugin.rb
+++ lib/nessusdb/models/plugin.rb
@@ -6,10 +6,10 @@ module NessusDB
 		#
 		# @author Jacob Hammack
 		class Plugin < ActiveRecord::Base
-			has_many :items
-			belongs_to :family
-			has_many :references
-			has_many :individualpluginselections
+			has_many :Items
+			belongs_to :Family
+			has_many :References
+			has_many :IndividualPluginSelections
 		
 			class << self
 				# Creates a graph based on the top plugins sorted by count
