e96cab09d03e380f712eb16b375b924dcc45a979
Jacob Hammack <jacob.hammack@hammackj.com>
2011-03-03 21:25:42 -0600

    ActiveRecord Model fix

--- lib/nessusdb/models/policy.rb
+++ lib/nessusdb/models/policy.rb
@@ -7,11 +7,11 @@ module NessusDB
 		#
 		# @author Jacob Hammack
 		class Policy < ActiveRecord::Base
-		  has_many :serverpreferences
-		  has_many :familyselections
-		  has_many :individualpluginselections
+		  has_many :family_selections
+		  has_many :individual_plugin_selections
 		  has_many :reports
-		  has_many :pluginspreferences
+		  has_many :plugins_preferences
+			has_many :server_preferences
 		end
 	end
 end
