0fab8296e608dd83cff77270689b023abf744346
Jacob Hammack <jacob.hammack@hammackj.com>
2011-03-04 18:48:34 -0600

    Added better test code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,20 @@
 base = __FILE__
 $:.unshift(File.join(File.dirname(base), '../lib'))
 
+
+require 'rspec'
 require 'nessusdb'
 require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 
-app = NessusDB::CLI::Application.new
-app.load_config
-app.db_connect
-app.migrate(:down)
-app.migrate(:up)
+begin
+	app = NessusDB::CLI::Application.new
+	app.load_config("test_data/nessusdb_spec.cfg")
+	app.db_connect
+	app.migrate(:down)
+	app.migrate(:up)
+rescue => e
+	puts "[!] #{e.message}\n#{e.backtrace}\n\n"
+end
 
 RSpec.configure do |config|
 	config.before(:all)    { Sham.reset(:before_all)  }
