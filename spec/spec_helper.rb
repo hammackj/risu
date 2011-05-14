d165589888332d9a61ff2e421d49cf64fa9f3785
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-13 20:11:38 -0500

    Added the ability to pass a yaml as a string for easier testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,21 @@ require 'rspec'
 require 'nessusdb'
 require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 
+config = 
+"report:
+  author: 
+  title: 
+  company: 
+  classification: 
+
+database:
+  adapter: sqlite3
+  database: test_data/test.db"
+
+
 begin
 	app = NessusDB::CLI::Application.new
-	app.load_config("test_data/nessusdb_spec.cfg")
+	app.load_config(config, true)
 	app.db_connect
 	app.migrate(:down)
 	app.migrate(:up)
