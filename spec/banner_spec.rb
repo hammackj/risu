dbfb88887cea3efa21580bada034b7c733209354
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-19 19:31:16 -0500

    Added a spec for the Banner Generation

---
+++ spec/banner_spec.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+module NessusDB
+	module CLI		
+		describe Banner do
+			before(:all) do
+			end
+			
+			it "should return a random banner" do				
+				Banner.to_s.length.should_not == 0
+			end
+		end
+	end
+end
