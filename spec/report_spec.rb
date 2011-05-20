910b54c3167e03cb0736ec8c8f133f48bae63bd7
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-19 23:18:58 -0500

    Added checks for 1.8.7 and 1.9.2

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -12,8 +12,15 @@ module NessusDB
 				printf "%s\n\n", @report.inspect
 			end
 			
-			it "should have a scan_date of Fri May 13 17:52:18 -0500 2011" do				
-				Report.scan_date.should == "Fri May 13 17:52:18 -0500 2011"
+			it "should have a scan_date of Fri May 13 17:52:18 -0500 2011" do
+				date = Report.scan_date
+				
+				#Ruby 1.8.7 and 1.9.2 return different date formats
+				if date == "Fri May 13 17:52:18 -0500 2011"
+					date.should == "Fri May 13 17:52:18 -0500 2011"
+				elsif date == "2011-05-13 17:52:18 -0500"
+					date.should == "2011-05-13 17:52:18 -0500"
+				end
 			end
 		end
 	end
