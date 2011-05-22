0b5224e259a385d54a9cdfffde84b6ac011d6b31
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-22 00:52:30 -0500

    Fixed a bug in the checking of the root document tag for version of the file

--- lib/nessusdb/nessusdocument.rb
+++ lib/nessusdb/nessusdocument.rb
@@ -1,51 +1,55 @@
 # encoding: utf-8
 
 module NessusDB
-	
+
 	# A Object to represet the Nessus xml file in memory
 	#
 	# @author Jacob Hammack <jacob.hammack@hammackj.com>
 	class NessusDocument
-		
+
 		# Creates a instance of the NessusDocument class
 		#
 		def initialize document
 			@document = document
 		end
-		
+
 		# Checks the validness of a NessusDocument
 		#
 		# @return [Boolean] True if valid, False if invalid
 		def valid?
-			@parser = LibXML::XML::Parser.file @document
-			doc = @parser.parse
-			
-			if doc == nil
-				return false
-			end
-			
-			node = doc.find("//NessusClientData_v2")
-			
-			if node.first.content != nil
-				return true
+			if File.exist?(@document)
+				@parser = LibXML::XML::Parser.file @document
+				doc = @parser.parse
+
+				if doc.root.name == nil
+					return false
+				end
+				
+				if doc.root.name == "NessusClientData_v2"
+					return true
+				elsif doc.root.name == "NessusClientData"
+					return false
+				else
+					return false
+				end
 			else
 				return false
 			end
 		end
-				
+
 		# Invokes the SAX parser on the XML document
 		#
 		def parse
 			@parser = LibXML::XML::SaxParser.file @document
-   		@parser.callbacks = NessusSaxListener.new
-   		@parser.parse
+			@parser.callbacks = NessusSaxListener.new
+			@parser.parse
 		end
-		
+
 		# Fixes the ip field if nil and replaces it with the name if its an ip
 		#
 		def fix_ips
 			@hosts = Host.all
-			
+
 			@hosts.each do |host|
 				if host.ip == nil
 					begin
@@ -57,6 +61,6 @@ module NessusDB
 					end
 				end
 			end
-		end		
+		end
 	end
 end
