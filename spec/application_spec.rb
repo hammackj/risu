396426e6fd4e3a8109c6f42425baba7fba621830
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-22 00:53:03 -0500

    Added some tests for Application class

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -3,6 +3,30 @@ require "spec_helper"
 module NessusDB
 	module CLI
 		describe Application do
+			before(:all) do
+				@app = Application.new
+					
+				printf "%s\n\n", @app.inspect						
+			end
+			
+			it "should create a config file for Application.create_config" do				
+				file_name = (1...25).map{65.+(rand(25)).chr}.join
+				@app.create_config("/tmp/#{file_name}")
+				sleep(1)
+				result = File.exist?("/tmp/#{file_name}")
+				File.delete("/tmp/#{file_name}") if result
+				result.should == true
+			end
+			
+			it "should return true for Application.test_connection" do
+				@app.test_connection?[0].should == true
+			end
+			
+			#parser_file
+			#run
+			#parse_options
+			#consolize
+			
 		end
 	end
 end
