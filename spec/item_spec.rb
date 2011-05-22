114da1a4b2c72196d8e46ef864a0fbac136de918
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-22 00:55:38 -0500

    Fixed spacing and tabs

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,24 +1,24 @@
 # encoding: utf-8
-
+ 	
 require 'spec_helper'
 
 module NessusDB
-	module Models		
+	module Models
 		describe Item do
 			before(:all) do
 				@high = Item.make(:severity => 3)
 				@med = Item.make(:severity => 2)
 				@low = Item.make(:severity => 1)
 				@info = Item.make(:severity => 0)
-					
+
 				printf "%s\n%s\n%s\n%s\n\n", @high.inspect, @med.inspect, @low.inspect, @info.inspect
-							
+
 			end
-			
-			it "should include high, medium, low and info risks for Item.risks.all" do				
+
+			it "should include high, medium, low and info risks for Item.risks.all" do
 				Item.risks.all.should include(@high, @med, @low, @info)
 			end
-			
+
 			it "returns 4 risks for Item.risks.count" do
 				Item.risks.count.should == 4
 			end
@@ -34,10 +34,10 @@ module NessusDB
 			it "returns 1 for Item.low_risks.count" do
 				Item.low_risks.all.count.should == 1
 			end
-			
+
 			it "returns 1 risks for Item.info_risks.count" do
 				Item.info_risks.all.count.should == 1
-			end			
+			end
 
 			it "returns 4 risks for Item.risks_by_service" do
 				Item.risks_by_service(100000).all.count.should == 4
@@ -54,7 +54,7 @@ module NessusDB
 			it "returns 1 for Item.high_risks_unique.count" do
 				Item.high_risks_unique.all.count.should == 1
 			end
-			
+
 			it "returns 1 for Item.high_risks_unique_sorted.count" do
 				Item.high_risks_unique_sorted.all.count.should == 1
 			end
@@ -62,7 +62,7 @@ module NessusDB
 			it "returns 1 for Item.medium_risks_unique.count" do
 				Item.medium_risks_unique.all.count.should == 1
 			end
-			
+
 			it "returns 1 for Item.medium_risks_unique_sorted.count" do
 				Item.medium_risks_unique_sorted.all.count.should == 1
 			end
@@ -70,34 +70,34 @@ module NessusDB
 			it "returns 1 for Item.low_risks_unique.count" do
 				Item.low_risks_unique.all.count.should == 1
 			end
-			
+
 			it "returns 1 for Item.low_risks_unique_sorted.count" do
 				Item.low_risks_unique_sorted.all.count.should == 1
 			end
-			
+
 			it "returns 1 for Item.info_risks_unique_sorted.count" do
 				Item.info_risks_unique_sorted.all.count.should == 1
 			end
-			
+
 			it "returns 1 for Item.info_risks_unique.count" do
 				Item.info_risks_unique.all.count.should == 1
 			end
-	
+
 			it "returns a graph Item.risks_by_severity_graph" do
 				Item.risks_by_severity_graph.class.should == StringIO
 			end
-			
+
 			it "returns a graph Item.risks_by_service_graph" do
 				Item.risks_by_service_graph.should be_a_kind_of StringIO
 			end
-			
+
 			it "returns 0 for Item.ms_update" do
 				Item.ms_update.all.count.should == 0
 			end
-			
+
 			it "returns 0 for Item.ms_patches" do
 				Item.ms_patches.all.count.should == 0
-			end						
+			end
 		end
 	end
 end
