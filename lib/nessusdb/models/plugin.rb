722aa6fc3c3590df41be8946963671030ba4d29d
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-22 00:38:35 -0500

    Fixed tabs and spacing

--- lib/nessusdb/models/plugin.rb
+++ lib/nessusdb/models/plugin.rb
@@ -2,7 +2,7 @@
 
 module NessusDB
 	module Models
-		
+
 		# Plugin Model
 		#
 		# @author Jacob Hammack
@@ -11,51 +11,51 @@ module NessusDB
 			belongs_to :family
 			has_many :references
 			has_many :individual_plugin_selections
-		
+
 			class << self
-				
+
 				# Queries for all risks based on Plugin.risk_factor
 				#
 				# @return [Array] of all risks
 				def risks
 					critical_risks + high_risks + medium_risks + low_risks + none_risks
 				end
-				
+
 				# Queries for all the critical risks based on Plugin.risk_factor
 				#
 				# @return [ActiveRelation] of Critical Risks
 				def critical_risks
 					where(:risk_factor => "Critical")
 				end
-				
+
 				#Queries for all the critical risks based on Plugin.risk_factor
 				#
 				# @return [ActiveRelation] of High Risks
 				def high_risks
 					where(:risk_factor => "High")
 				end
-				
+
 				# Queries for all the critical risks based on Plugin.risk_factor
 				#
 				# @return [ActiveRelation] of Medium Risks
 				def medium_risks
 					where(:risk_factor => "Medium")
 				end
-				
+
 				# Queries for all the critical risks based on Plugin.risk_factor
 				#
 				# @return [ActiveRelation] of Low Risks
 				def low_risks
 					where(:risk_factor => "Low")
 				end
-				
+
 				# Queries for all the critical risks based on Plugin.risk_factor
 				#
 				# @return [ActiveRelation] of None Risks
 				def none_risks
 					where(:risk_factor => "None")
 				end
-				
+
 				# Creates a graph based on the top plugins sorted by count
 				#
 				# @return Filename of the created graph
@@ -67,10 +67,10 @@ module NessusDB
 						:colors => %w(red green blue orange yellow purple black grey brown pink),
 						:background_colors => %w(white white)
 					}
-					
-					Item.risks_by_plugin(limit).all.each do |plugin|					
+
+					Item.risks_by_plugin(limit).all.each do |plugin|
 						plugin_name = Plugin.find_by_id(plugin.plugin_id).plugin_name
-						
+
 						#We need to filter the names a little to make everything look nice on the graph
 						plugin_name = case plugin.plugin_id
 							when 35362 then plugin_name.split(":")[0]
@@ -89,7 +89,7 @@ module NessusDB
 
 						g.data(plugin_name, Item.where(:plugin_id => plugin.plugin_id).count)
 					end
-				
+
 					StringIO.new(g.to_blob)
 				end
 			end
