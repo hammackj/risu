81f06d5f90c0174d288cbcb50e76d69f09aaff78
Jacob Hammack <jacob.hammack@hammackj.com>
2011-05-23 22:06:05 -0500

    Added clean up to see if I can auto have it clean all the tables

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,20 @@ RSpec.configure do |config|
 	config.before(:each) do 
 		Sham.reset 
 	end
+	
+	config.after(:all) do
+		Report.delete_all
+		Host.delete_all
+		Plugin.delete_all
+		Item.delete_all
+		FamilySelection.delete_all
+		IndividualPluginSelection.delete_all
+		PluginsPreference.delete_all
+		Policy.delete_all
+		Reference.delete_all
+		ServerPreference.delete_all
+		Version.delete_all
+	end
 end
 
 CoverMe.config do |c|
