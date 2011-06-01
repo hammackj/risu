84f0ed48f062d7f1c9403236e381ff9b7ee86a6f
Jacob Hammack <jacob.hammack@hammackj.com>
2011-06-01 17:42:20 -0500

    Added a base template class for the new template system

---
+++ lib/risu/base/template_base.rb
@@ -0,0 +1,22 @@
+module Risu
+	module Base
+  	#
+		#
+		class TemplateBase
+	    @possible_templates = []
+	    class << self
+	      attr_reader :possible_templates
+	    end
+    	
+			#
+			#
+			attr_accessor :template_info
+			
+			#
+			#
+	    def self.inherited(child)
+	      possible_templates << child
+	    end
+	  end
+	end
+end
