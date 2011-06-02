2ad299135ac788b16c73186b3db97e1cab81b652
Jacob Hammack <jacob.hammack@hammackj.com>
2011-06-01 22:55:29 -0500

    Fixing output variable issues.

--- lib/risu/templates/assets.rb
+++ lib/risu/templates/assets.rb
@@ -17,29 +17,29 @@ module Risu
 			#
 			#
 			def render(output)
-				text Report.classification, :align => :center
-				text "\n"
+				output.text Report.classification, :align => :center
+				output.text "\n"
 
-				font_size(22) { 
-					text Report.title, :align => :center 
+				output.font_size(22) { 
+					output.text Report.title, :align => :center 
 				}
 				
-				font_size(18) { 
-					text "Networked Assets", :align => :center
-					text "\n"
-					text "This report was prepared by\n#{Report.author}", :align => :center
+				output.font_size(18) { 
+					output.text "Networked Assets", :align => :center
+					output.text "\n"
+					output.text "This report was prepared by\n#{Report.author}", :align => :center
 				}
 
-				text "\n\n"
+				output.text "\n\n"
 
 				Host.sorted.each do |host|
-					text "Name: #{host.name}\n"
-					text "FQDN: #{host.fqdn}\n" unless host.fqdn == nil
-					text "IP Address: #{host.ip}\n" unless host.ip == nil
-					text "NetBios: #{host.netbios}\n" unless host.netbios == nil
-					text sprintf "Mac Address: %s\n", host.mac.chomp.gsub("\n", ", ") unless host.mac == nil
-					text sprintf "Operation System: %s\n", host.os.chomp.gsub("\n", "/") unless host.os == nil
-					text "\n"
+					output.text "Name: #{host.name}\n"
+					output.text "FQDN: #{host.fqdn}\n" unless host.fqdn == nil
+					output.text "IP Address: #{host.ip}\n" unless host.ip == nil
+					output.text "NetBios: #{host.netbios}\n" unless host.netbios == nil
+					output.text sprintf "Mac Address: %s\n", host.mac.chomp.gsub("\n", ", ") unless host.mac == nil
+					output.text sprintf "Operation System: %s\n", host.os.chomp.gsub("\n", "/") unless host.os == nil
+					output.text "\n"
 				end
 			end
 		end
