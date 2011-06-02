fdd9426f5ddfa81a5b377e1a47876a8f37e42a56
Jacob Hammack <jacob.hammack@hammackj.com>
2011-06-01 22:56:18 -0500

    Updated for the new templates

--- test_data/template_test.sh
+++ test_data/template_test.sh
@@ -1,40 +1,42 @@
 #!/usr/bin/env bash
 
+rm *.pdf
+
 echo "assets" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/assets.rb -o assets.pdf &&
+../bin/risu --config-file test.cfg -t assets -o assets.pdf &&
 
 echo "stats" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/finding_statistics.rb -o stats.pdf &&
+../bin/risu --config-file test.cfg -t finding_statistics -o stats.pdf &&
 
 echo "graphs" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/graphs.rb -o graphs.pdf &&
+../bin/risu --config-file test.cfg -t graphs -o graphs.pdf &&
 
 echo "pci" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/pci_compliance.rb -o pci.pdf &&
+../bin/risu --config-file test.cfg -t pci_compliance -o pci.pdf &&
 
 echo "tech" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/technical_findings.rb -o tech.pdf &&
+../bin/risu --config-file test.cfg -t technical_findings -o tech.pdf &&
 
 echo "exec_summary" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/exec_summary.rb -o exec.pdf &&
+../bin/risu --config-file test.cfg -t exec_summary -o exec.pdf &&
 
 echo "findings_summary" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/findings_summary.rb -o findings_summary.pdf &&
+../bin/risu --config-file test.cfg -t findings_summary -o findings_summary.pdf &&
 
 echo "host_summary" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/host_summary.rb -o host_summary.pdf &&
+../bin/risu --config-file test.cfg -t host_summary -o host_summary.pdf &&
 
 echo "ms_patch_summary" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/ms_patch_summary.rb -o patches.pdf &&
+../bin/risu --config-file test.cfg -t ms_patch_summary -o patches.pdf &&
 
 echo "ms_update_summary" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/ms_update_summary.rb -o updates.pdf &&
+../bin/risu --config-file test.cfg -t ms_update_summary -o updates.pdf &&
 
 echo "findings_host" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/findings_host.rb -o findings_host.pdf &&
+../bin/risu --config-file test.cfg -t findings_host -o findings_host.pdf &&
 
 echo "cover_sheet" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/cover_sheet.rb -o cover_sheet.pdf &&
+../bin/risu --config-file test.cfg -t cover_sheet -o cover_sheet.pdf &&
 
 echo "executive_summary" &&
-../bin/risu --config-file test.cfg -t ../lib/risu/templates/executive_summary.rb -o executive_summary.pdf
+../bin/risu --config-file test.cfg -t executive_summary -o executive_summary.pdf
