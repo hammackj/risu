88a1cdb63048a426291cd254df3937e34e276870
Jacob Hammack <jacob.hammack@hammackj.com>
2011-06-01 19:54:06 -0500

    Ported to new template format.

--- lib/risu/templates/cover_sheet.rb
+++ lib/risu/templates/cover_sheet.rb
@@ -1,42 +1,68 @@
-image File.dirname(template) + "/data/nessuslogo.jpg", :scale => 0.2, :position => :left, :vposition => :top
+module NaslTidy
+	module Modules
+		class CoverSheet < Risu::Base::TemplateBase
+			
+			#
+			#
+			def initialize ()
+				@template_info = 
+				{ 
+					:name => "cover_sheet", 
+					:author => "hammackj", 
+					:version => "0.0.1", 
+					:description => "Generates a coversheet with a logo (Example Template)"
+				}
+			end
+			
+			#
+			#
+			def render
+				image File.dirname(template) + "/data/nessuslogo.jpg", :scale => 0.2, :position => :left, :vposition => :top
 
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
 
-font_size(24) { text Report.title, :align => :center }
+				font_size(24) { text Report.title, :align => :center }
 
-font_size(18) { 
-    text "Coversheet Example", :align => :center
-    text "\n"
-    text "This report was prepared by\n#{Report.author}", :align => :center
-}
+				font_size(18) { 
+				    text "Coversheet Example", :align => :center
+				    text "\n"
+				    text "This report was prepared by\n#{Report.author}", :align => :center
+				}
 
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
-text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
 
-text "Nessus is a Registered Trademark of Tenable Network Security, Inc."
+				text "Nessus is a Registered Trademark of Tenable Network Security, Inc."
+			end
+		end
+	end
+end
+
+
+
