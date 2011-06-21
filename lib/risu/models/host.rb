b2c1ef42ec296c7d328f3791d5c3f41a1b99f992
Jacob Hammack <jacob.hammack@hammackj.com>
2011-06-20 22:19:36 -0500

    Added graph text functions

--- lib/risu/models/host.rb
+++ lib/risu/models/host.rb
@@ -45,7 +45,7 @@ module Risu
 				def os_windows
 					where("os LIKE '%Windows%'")#.group(:os)
 				end
-				
+
 				# Negation query for all hosts with a Windows based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -59,21 +59,21 @@ module Risu
 				def os_windows_nt
 					where("os LIKE '%Windows NT%'")
 				end
-				
+
 				# Negation query for all hosts with a Windows NT based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
 				def not_os_windows_nt
 					where("os NOT LIKE '%Windows NT%'")
 				end
-				
+
 				# Queries for hosts with a Windows 2000 based Operating System
 				#
 				# @return [ActiveRecord::Relation] with the query results
 				def os_windows_2k
 					where("os LIKE '%Windows 2000%'")
 				end
-							
+
 				# Negation query for all hosts with a Windows 2000 based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -87,7 +87,7 @@ module Risu
 				def os_windows_xp
 					where("os LIKE '%Windows XP%'")
 				end
-				
+
 				# Negation query for all hosts with a Windows XP based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -101,7 +101,7 @@ module Risu
 				def os_windows_2k3
 					where("os LIKE '%Windows Server 2003%'")
 				end
-				
+
 				# Negation query for all hosts with a Windows Server 2003 based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -115,7 +115,7 @@ module Risu
 				def os_windows_vista
 					where("os LIKE '%Windows Vista%'")
 				end
-				
+
 				# Negation query for all hosts with a Windows Vista based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -129,7 +129,7 @@ module Risu
 				def os_windows_2k8
 					where("os LIKE '%Windows Server 2008%'")
 				end
-				
+
 				def not_os_windows_2k8
 					where("os NOT LIKE '%Windows Server 2008%'")
 				end
@@ -140,7 +140,7 @@ module Risu
 				def os_windows_7
 					where("os LIKE '%Windows 7%'")
 				end
-				
+
 				# Negation query for all hosts with a Windows 7 based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -155,14 +155,14 @@ module Risu
 				def os_windows_other
 					not_os_windows_7.not_os_windows_2k8.not_os_windows_vista.not_os_windows_2k3.not_os_windows_xp.not_os_windows_2k.not_os_windows_nt
 				end
-				
+
 				# Queries for all hosts with a Linux based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
 				def os_linux
 					where("os LIKE '%Linux%'")
 				end
-				
+
 				# Negation query for all hosts with a Linux based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -176,7 +176,7 @@ module Risu
 				def os_freebsd
 					where("os LIKE '%FreeBSD%'")
 				end
-				
+
 				# Negation query for all hosts with a FreeBSD based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -190,7 +190,7 @@ module Risu
 				def os_netbsd
 					where("os LIKE '%NetBsd%'")
 				end
-				
+
 				# Negation query for all hosts with a NETbsd based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -204,7 +204,7 @@ module Risu
 				def os_cisco
 					where("os LIKE '%CISCO%'")
 				end
-				
+
 				# Negation query for all hosts with a Cisco based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -218,7 +218,7 @@ module Risu
 				def os_vxworks
 					where("os LIKE '%VxWorks%'")
 				end
-				
+
 				# Negation query for all hosts with a VXWorks based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
@@ -232,32 +232,32 @@ module Risu
 				def os_vmware_esx
 					where("os LIKE '%VMware ESX%'")
 				end
-				
+
 				# Negation query for all hosts with a VMware ESX based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
 				def not_os_vmware_esx
 					where("os NOT LIKE '%VMware ESX%'")
 				end
-				
+
 				# Queries for all hosts with a Mac OSX based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
 				def os_osx
 					where("os LIKE '%Mac OS X%'")
 				end
-				
+
 				# Negation query for all hosts with a Mac OSX based Operating system
 				#
 				# @return [ActiveRecord::Relation] with the query results
 				def not_os_osx
 					where("os NOT LIKE '%Mac OS X%'")
 				end
-				
+
 				def os_aix
 					where("os LIKE '%AIX%'")
 				end
-				
+
 				def not_os_aix
 					where("os NOT LIKE '%AIX%'")
 				end
@@ -325,7 +325,7 @@ module Risu
 
 					#Creates very odd graphs
 					#Host.os_other.each do |host|
-					#	g.data(host.os, Host.where(:os => host.os).count) unless host.os == nil
+					# g.data(host.os, Host.where(:os => host.os).count) unless host.os == nil
 					#end
 
 					StringIO.new(g.to_blob)
@@ -364,54 +364,120 @@ module Risu
 					StringIO.new(g.to_blob)
 				end
 				
+				#
+				#
+				def windows_os_graph_text
+					nt = Host.os_windows_nt.all.count
+					w2k = Host.os_windows_2k.all.count
+					xp = Host.os_windows_xp.all.count
+					w2k3 = Host.os_windows_2k3.all.count
+					vista = Host.os_windows_vista.all.count
+					w2k8 = Host.os_windows_2k8.all.count
+					w7 = Host.os_windows_7.all.count
+					other = (Host.os_windows.os_windows_other).all.count
+					
+					windows_os_count = nt + w2k + xp + w2k3 + vista + w7 + w2k8
+					
+					nt_percent = (nt.to_f / windows_os_count.to_f) * 100
+					w2k_percent = (w2k.to_f / windows_os_count.to_f) * 100
+					xp_percent = (xp.to_f / windows_os_count.to_f) * 100
+					w2k3_percent = (w2k3.to_f / windows_os_count.to_f) * 100
+					vista_percent = (vista.to_f / windows_os_count.to_f) * 100
+					w2k8_percent = (w2k8.to_f / windows_os_count.to_f) * 100
+					w7_percent = (w7.to_f / windows_os_count.to_f) * 100
+					
+					text = "This graph shows the percentage of the different Microsoft Windows based operating systems " +
+					"found on the #{Report.title} network.\n\n"
+					
+					text << "#{nt_percent.round.to_i}% of the network is Windows NT. " if nt > 0
+					text << "#{w2k_percent.round.to_i}% of the network is Windows 2000. " if w2k > 0
+					text << "#{xp_percent.round.to_i}% of the network is Windows XP. " if xp > 0
+					text << "#{w2k3_percent.round.to_i}% of the network is Windows Server 2003. " if w2k3 > 0
+					text << "#{vista_percent.round.to_i}% of the network is Windows Vista. " if vista > 0
+					text << "#{w2k8_percent.round.to_i}% of the network is Windows Server 2008. " if w2k8 > 0
+					text << "#{w7_percent.round.to_i}% of the network is Windows 7. " if w7 > 0
+					
+					text << "\n\n" << unsupported_os_windows if nt > 0 or w2k > 0
+					
+					return text					
+				end
+
 				# @todo add plural check
 				#
 				def unsupported_os_text
 					aix_text = unsupported_os_aix
 					win_text = unsupported_os_windows
-											
-					unsupported_os_text = "Several unsupported Operating Systems have been discovered on the network. " +
-					"These Operating System are no longer updated by the specific vendor. These operating systems should be " +
+
+					unsupported_os_text = "Several unsupported operating systems were also discovered on the network. " +
+					"These operating systems are no longer updated by the specific vendor. These operating systems should be " +
 					"updated and replaced as soon as possible.\n\n"
-					
-					unsupported_os_text << "#{win_text}\n" if win_text != ""
-					unsupported_os_text << "#{aix_text}\n" if aix_text != ""
+
+					unsupported_os_text << "#{win_text}" if win_text != ""
+					unsupported_os_text << "#{aix_text}" if aix_text != ""
 					unsupported_os_text << "\n"
-					
+
 					return unsupported_os_text
 				end
-				
+
 				def unsupported_os_windows
 					win_nt_text = ""
 					win_2000_text = ""
 					win_nt = Host.os_windows_nt
 					win_2000 = Host.os_windows_2k
-					
+
 					#Host.os_windows.not_os_windows_7.not_os_windows_2008.not_os_windows_vista.not_os_windows_2003.not_os_windows_xp
-					
-					win_nt_text = "Windows NT is an unsupported Operating System; Microsoft has stopped support as of June 2004. " +
-					"Please see http://windows.microsoft.com/en-us/windows/products/lifecycle for more information.\n" if win_nt.count >= 1
-					
-					win_2000_text = "Windows 2000 is an unsupported Operating System; Microsoft has stopped support as of June 2004. " +
-					"Please see http://windows.microsoft.com/en-us/windows/products/lifecycle for more information.\n" if win_2000.count >= 1 
-					
+
+					win_nt_text = "Windows NT is an unsupported sperating system, since Microsoft has stopped support as of June 2004. " +
+					"Please see http://windows.microsoft.com/en-us/windows/products/lifecycle for more information.\n\n" if win_nt.count >= 1
+
+					win_2000_text = "Windows 2000 is an unsupported operating system, since Microsoft has stopped support as of June 2004. " +
+					"Please see http://windows.microsoft.com/en-us/windows/products/lifecycle for more information.\n\n" if win_2000.count >= 1
+
 					return "#{win_nt_text}#{win_2000_text}"
-					
+
 				end
-				
+
 				#
 				#
 				def unsupported_os_aix
 					text = ""
 					aix = Host.os_aix.where("OS LIKE 'AIX 5.%'")
-					
-					text = "AIX 5.x is an unsupported Operating System; IBM has stopped support as of April 2011. " +
+
+					text = "AIX 5.x is an unsupported operating system, since IBM has stopped support as of April 2011. " +
 					"Please see http://www-03.ibm.com/systems/power/software/aix/ for more information " +
-					"about obtaining a newer supported version." if aix.count >= 1
-					
+					"about obtaining a newer supported version.\n\n" if aix.count >= 1
+
 					return text
 				end
-				
+
+				def other_os_graph_text
+					text = "This graph shows the percentage of the different Non-Windows based operating systems " +
+					"found on the #{Report.title} network.\n\n"
+
+					linux = Host.os_linux.all.count
+					osx = Host.os_osx.all.count
+					freebsd = Host.os_freebsd.all.count
+					netbsd = Host.os_netbsd.all.count
+					cisco = Host.os_cisco.all.count
+					vxworks = Host.os_vxworks.all.count
+					esx = Host.os_vmware_esx.all.count
+					aix = Host.os_aix.all.count
+					other = Host.os_other.all.count
+
+					other_os_count = linux + osx + freebsd + netbsd + cisco + vxworks + esx + aix + other
+
+					linux_percent = (linux.to_f / other_os_count.to_f) * 100
+					aix_percent = (aix.to_f / other_os_count.to_f) * 100
+					
+					#todo add other os's here
+					
+
+					text << "#{linux_percent.to_i}% of the network is running Linux based operating systems. " if linux > 0
+					text << "#{aix_percent.to_i}% of the network is running AIX based operating systems. " if aix > 0
+					text << "\n\n"<< unsupported_os_aix if aix > 0
+
+					return text
+				end
 			end
 		end
 	end
