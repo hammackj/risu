d90e379dd87f371b066a29264cb182b8405f5489
Jacob Hammack <jacob.hammack@hammackj.com>
2012-01-23 16:13:56 -0600

    Removed dead code.

--- lib/risu/base.rb
+++ lib/risu/base.rb
@@ -3,11 +3,6 @@ module Risu
 	end
 end
 
-#Remove this by 1.5 as it doesn't seem to be needed
-#if ActiveRecord::Base.connected? == true
-#	require 'risu/base/schema'
-#end
-
 require 'risu/base/template_base'
 require 'risu/base/template_manager'
 require 'risu/base/prawn_templater'
