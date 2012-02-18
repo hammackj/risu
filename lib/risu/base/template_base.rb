3c72aff9fd74100b1a1079f17de88e2a3ab83233
Jacob Hammack <jacob.hammack@hammackj.com>
2012-02-18 14:40:31 -0600

    Spaces

--- lib/risu/base/template_base.rb
+++ lib/risu/base/template_base.rb
@@ -1,26 +1,26 @@
 module Risu
 	module Base
 
-  	# Base template class, all report templates must be a subclass of this.
+		# Base template class, all report templates must be a subclass of this.
 		#
 		class TemplateBase			
-	    @possible_templates = []
+			@possible_templates = []
 	
-	    class << self
-	      attr_reader :possible_templates
-	    end
-    	
+			class << self
+				attr_reader :possible_templates
+			end
+			
 			# Accessor for template metadata
 			#
 			# @return [Hash] Containing template metadata
 			attr_accessor :template_info
 			
-			#  Adds any class that inherits from [TemplateBase] into an [Array] of
+			#	 Adds any class that inherits from [TemplateBase] into an [Array] of
 			# possible templates for further validation.
 			#			
-	    def self.inherited(child)
-	      possible_templates << child
-	    end
-	  end
+			def self.inherited(child)
+				possible_templates << child
+			end
+		end
 	end
 end
