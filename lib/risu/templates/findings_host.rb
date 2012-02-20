6eb246b059e99c837deb997f67e69f5ab5614dcc
Jacob Hammack <jacob.hammack@hammackj.com>
2012-02-19 22:00:00 -0600

    Added critical risk and lowered the font to save paper

--- lib/risu/templates/findings_host.rb
+++ lib/risu/templates/findings_host.rb
@@ -9,7 +9,7 @@ module Risu
 				{ 
 					:name => "findings_host", 
 					:author => "hammackj", 
-					:version => "0.0.1", 
+					:version => "0.0.2", 
 					:description => "Generates a findings report by host"
 				}
 			end
@@ -17,12 +17,14 @@ module Risu
 			#
 			#
 			def render(output)
+				output.font_size 10
+				
 				output.text Report.classification.upcase, :align => :center
 				output.text "\n"
 
 				output.font_size(22) { output.text Report.title, :align => :center }
 				output.font_size(18) { 
-					output.text "Findings Summary Report", :align => :center
+					output.text "Findings Summary by Host Report", :align => :center
 					output.text "\n"
 					output.text "This report was prepared by\n#{Report.author}", :align => :center
 				}
@@ -31,13 +33,30 @@ module Risu
 
 				Host.sorted.each do |host|
 					if host.items.high_risks_unique_sorted.all.size > 0 or host.items.medium_risks_unique_sorted.all.size > 0
-						output.font_size(20) { 
-							output.text "#{host.ip} - #{host.fqdn}", :style => :bold 
-							}
+						output.font_size(16) do
+							
+							host_string = "#{host.ip}"
+							host_string << " (#{host.fqdn})" if host.fqdn != nil
+							
+							output.text "#{host_string}", :style => :bold 
+						end
+					end
+
+					if host.items.critical_risks_unique_sorted.all.size > 0
+						output.font_size(12) do 
+							output.fill_color "551A8B"
+							output.text "Critical Findings", :style => :bold 
+							output.fill_color "000000"
+						end
+
+						host.items.high_risks_unique_sorted.each do |item|
+							name = Plugin.find_by_id(item.plugin_id).plugin_name
+							output.text "#{name}"
+						end
 					end
 
 					if host.items.high_risks_unique_sorted.all.size > 0
-						output.font_size(18) { 
+						output.font_size(12) { 
 							output.fill_color "FF0000"
 							output.text "High Findings", :style => :bold 
 							output.fill_color "000000"
@@ -50,7 +69,7 @@ module Risu
 					end
 
 					if host.items.medium_risks_unique_sorted.all.size > 0
-						output.font_size(18) { 
+						output.font_size(12) { 
 							output.fill_color "FF8040"
 							output.text "Medium Findings", :style => :bold 
 							output.fill_color "000000"
