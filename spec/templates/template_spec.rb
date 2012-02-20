958459d03454a821a5070bcfab207acf24c3ae27
Jacob Hammack <jacob.hammack@hammackj.com>
2012-02-20 00:04:54 -0600

    Added a spec for the template, hehe

---
+++ spec/templates/template_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+module Risu
+	module Templates		
+		describe Template do
+			before(:all) do
+				@file_name = "/tmp/template.pdf"
+				@template_manager = Risu::Base::TemplateManager.new "risu/templates"
+				@templater = Risu::Base::Templater.new("template", Report, @file_name, @template_manager)
+				
+				@report = Report
+				@report.title = "Rspec Test"
+				@report.author = "hammackj"
+				@report.company = "None"
+				@report.classification = "None"
+				
+				@host = Host.make
+				@host1 = Host.make
+			end
+			
+			after(:all) do
+				Report.delete_all
+				Host.delete_all
+				Plugin.delete_all
+				Item.delete_all
+				FamilySelection.delete_all
+				IndividualPluginSelection.delete_all
+				PluginsPreference.delete_all
+				Policy.delete_all
+				Reference.delete_all
+				ServerPreference.delete_all
+				Version.delete_all
+				
+#				File.delete(@file_name) if File.exist?(@file_name)
+			end
+			
+			it "should create #{@file_name} on template creation" do				
+				@templater.generate
+				File.exist?(@file_name).should == true
+			end
+			
+			it "should have an MD5 of 4f30a246e405d570b320fa8020092ec8 after creation" do
+				if File.exist?(@file_name) == true
+					require 'digest/md5'
+					Digest::MD5.hexdigest(File.read(@file_name)).should == "4f30a246e405d570b320fa8020092ec8"
+				end
+			end
+		end
+	end
+end
