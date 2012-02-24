8c596f32d6593ca010f8c50cf348566ad80e6429
Jacob Hammack <jacob.hammack@hammackj.com>
2012-02-23 23:40:15 -0600

    Added critical data

--- lib/risu/templates/findings_summary_with_pluginid.rb
+++ lib/risu/templates/findings_summary_with_pluginid.rb
@@ -9,7 +9,7 @@ module Risu
 				{ 
 					:name => "findings_summary_with_pluginid", 
 					:author => "hammackj", 
-					:version => "0.0.1", 
+					:version => "0.0.2", 
 					:description => "Geneates a Findings Summary with Nessus Plugin ID"
 				}
 			end
@@ -33,10 +33,26 @@ module Risu
 				output.text "\n\n\n"
 
 				output.font_size(20) { 
+					output.fill_color "9B30FF"
+					output.text "Critical Findings", :style => :bold 
+					output.fill_color "000000"
+				}
+
+				Item.critical_risks_unique_sorted.each do |item|	
+					name = Plugin.find_by_id(item.plugin_id).plugin_name
+					count = Item.where(:plugin_id => item.plugin_id).count
+
+					output.text "#{count} - #{name} - #{item.plugin_id}"
+				end
+
+				output.start_new_page
+
+
+				output.font_size(20) do
 					output.fill_color "FF0000"
 					output.text "High Findings", :style => :bold 
 					output.fill_color "000000"
-				}
+				end
 
 				Item.high_risks_unique_sorted.each do |item|
 					name = Plugin.find_by_id(item.plugin_id).plugin_name
