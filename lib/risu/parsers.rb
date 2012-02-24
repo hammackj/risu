93cca0c97dda2d63acbb578220be47c8c4edd2bc
Jacob Hammack <jacob.hammack@hammackj.com>
2012-02-23 23:37:39 -0600

    Starting to implement other parsers

--- lib/risu/parsers.rb
+++ lib/risu/parsers.rb
@@ -5,3 +5,6 @@ end
 
 require 'risu/parsers/nessus/nessus_document'
 require 'risu/parsers/nessus/nessus_sax_listener'
+
+require 'risu/parsers/nexpose/nexpose_document'
+require 'risu/parsers/nexpose/simple_nexpose'
