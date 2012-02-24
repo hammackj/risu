27c35500f4ead7f08d4325c40160861e839c7b47
Jacob Hammack <jacob.hammack@hammackj.com>
2012-02-23 23:37:11 -0600

    Added hash based test

--- test/functional/technical_findings_template_test.rb
+++ test/functional/technical_findings_template_test.rb
@@ -5,13 +5,15 @@ class TechnicalFindingsTemplateTest < ActiveSupport::TestCase
 	def setup
 		@file_name = "/tmp/tech_find.pdf"
 		@template_manager = Risu::Base::TemplateManager.new "risu/templates"
-		@templater = Risu::Base::Templater.new("technical_findings", Report, @file_name, @template_manager)
-		
+
 		@report = Report
 		@report.title = "Function Test"
 		@report.author = "hammackj"
 		@report.company = "None"
 		@report.classification = "None"
+		
+		@templater = Risu::Base::Templater.new("technical_findings", Report, @file_name, @template_manager)
+		@templater.generate
 	end
 	
 	def teardown
@@ -19,7 +21,11 @@ class TechnicalFindingsTemplateTest < ActiveSupport::TestCase
 	end
 	
 	test "should create #{@filename} on template creation" do
-		@templater.generate
 		assert File.exist?(@file_name) == true
+	end
+	
+	test "should have an MD5 of 39743ac03e9085e9e0242803e9b3fae0 after creation" do
+		require 'digest/md5'
+		assert Digest::MD5.hexdigest(File.read(@file_name)) == "39743ac03e9085e9e0242803e9b3fae0", "GOT #{Digest::MD5.hexdigest(File.read(@file_name))}"
 	end
 end
