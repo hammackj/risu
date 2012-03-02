4eb1836fc7ca933d03ed8c0200fb5f74a4c3aec5
Jacob Hammack <jacob.hammack@hammackj.com>
2012-03-02 00:53:55 -0600

    Added Full Sinatra base app for the webgui

--- lib/risu/web/application.rb
+++ lib/risu/web/application.rb
@@ -1,55 +1,38 @@
-#!/usr/bin/env ruby
+module Risu
+	module Web
+		class Application < Sinatra::Base
+			set :port, 8969
+			set :haml, :format => :html5
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '/../lib'))
+			get '/' do	
+				haml :index
+			end
 
-require 'rubygems'
-require 'risu'
-require 'sinatra'
+			get '/hosts' do
+				haml :hosts
+			end
 
-config = 
-"report:
-  author: TEST
-  title: TEST
-  company: TEST
-  classification: TEST
+			get '/host/:id' do
+				@host = Host.find(params[:id])
+				haml :host, :layout => :layout
+			end
 
-database:
-  adapter: sqlite3
-  database: test.db"
+			get '/reports' do
+				haml :reports
+			end
 
-begin
-	@app = Risu::CLI::Application.new
-	@app.load_config(config, true)
-	@app.db_connect
-rescue => e
-	puts "[!] #{e.message}\n#{e.backtrace}\n\n"
+			get '/report/:id' do
+				@report = Host.find(params[:id])
+				haml :report
+			end
+
+			get '/items' do
+				haml :items
+			end
+			
+			get '/plugins' do
+				haml :plugins
+			end
+		end
+	end
 end
-
-set :haml, :format => :html5
-
-get '/' do	
-	haml :index
-end
-
-get '/hosts' do
-	haml :hosts
-end
-
-get '/host/:id' do
-	@host = Host.find(params[:id])
-	haml :host, :layout => :layout
-end
-
-get '/reports' do
-	haml :reports
-end
-
-get '/report/:id' do
-	@report = Host.find(params[:id])
-	haml :report
-end
-
-get '/items' do
-	haml :items
-end
-
