0b84ad5a3c898dff5d04c30a28899db5a93b78ab
Jacob Hammack <jacob.hammack@hammackj.com>
2012-07-05 22:02:22 -0500

    Spelling error

--- lib/risu/templates/exec_summary.rb
+++ lib/risu/templates/exec_summary.rb
@@ -1,19 +1,19 @@
 module Risu
 	module Templates
 		class ExecSummary < Risu::Base::TemplateBase
-			
+
 			#
 			#
 			def initialize ()
-				@template_info = 
-				{ 
-					:name => "exec_summary", 
-					:author => "hammackj", 
-					:version => "0.0.2", 
+				@template_info =
+				{
+					:name => "exec_summary",
+					:author => "hammackj",
+					:version => "0.0.3",
 					:description => "Generates a simple executive summary."
 				}
 			end
-			
+
 			#
 			#
 			def render(output)
@@ -21,9 +21,9 @@ module Risu
 				output.text "\n"
 
 				output.font_size(22) do
-					output.text Report.title, :align => :center 
+					output.text Report.title, :align => :center
 				end
-				
+
 				output.font_size(18) do
 				    output.text "Executive Summary", :align => :center
 				    output.text "\n"
@@ -38,7 +38,7 @@ module Risu
 
 				output.text "This report contains the results of a security audit performed on #{Report.scan_date}. It contains confidential information about the state of your network. Access to this information by unauthorized personnel may allow them to compromise your network.\n\n"
 
-				output.text "A total of #{Host.count} hosts were found and scanned for vulnerabities.\n\n"
+				output.text "A total of #{Host.count} hosts were found and scanned for vulnerabilities.\n\n"
 
 				output.text "There were #{Item.risks.count} vulnerabilities found during this scan. Of these, #{Item.critical_risks.count} were critical vulnerabilities,#{Item.high_risks.count} were high vulnerabilities, #{Item.medium_risks.count} were medium vulnerabilities, #{Item.low_risks.count} were low vulnerabilities and #{Item.info_risks.count} were information findings.\n\n"
 
@@ -46,21 +46,21 @@ module Risu
 				output.text "\n"
 
 				output.table([[
-					"Number of hosts", 
+					"Number of hosts",
 					"Number of risks",
 					"Critical Risks",
 					"High Risks",
 					"Medium Risks",
 					"Low Risks",
-					"Info Risks"], 
+					"Info Risks"],
 				[
-					Host.count, 
+					Host.count,
 					Item.risks.count,
 					Item.critical_risks.count,
-					Item.high_risks.count, 
-					Item.medium_risks.count, 
-					Item.low_risks.count, 
-					Item.info_risks.count]], 
+					Item.high_risks.count,
+					Item.medium_risks.count,
+					Item.low_risks.count,
+					Item.info_risks.count]],
 				:cell_style => { :padding =>12 }, :width => output.bounds.width)
 				output.text "\n\n\n"
 
@@ -71,7 +71,7 @@ module Risu
 				output.image Item.risks_by_severity_graph, :width => 250, :at => [output.bounds.left, cury]
 				output.image Host.top_vuln_graph(10), :width => 250, :at => [output.bounds.right - 250, cury]
 				output.move_down 50
-				if (output.y <= 300) 
+				if (output.y <= 300)
 				    output.start_new_page
 						output.move_down 75
 				end
@@ -79,7 +79,7 @@ module Risu
 				output.image Item.risks_by_service_graph(10), :width => 250, :at => [output.bounds.left, cury]
 				output.image Host.other_os_graph, :width => 250, :at => [output.bounds.right - 250, cury]
 				output.move_down 250
-				#if (y <= 300) 
+				#if (y <= 300)
 				#    output.start_new_page
 				#		cury = y
 				#end
