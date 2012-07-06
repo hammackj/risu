8e888ca205780b2b2483bad61b23d0b3ac775718
Jacob Hammack <jacob.hammack@hammackj.com>
2012-07-05 21:52:22 -0500

    Dropping support for ruby 1.8.x

--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
- 
-class ReportTest < ActiveSupport::TestCase	
+
+class ReportTest < ActiveSupport::TestCase
 	test "should have a scan_date of Fri May 13 17:52:18 -0500 2011" do
 		date = Report.scan_date
 
@@ -8,11 +8,11 @@ class ReportTest < ActiveSupport::TestCase
 		#Ruby 1.8.7 and 1.9.2 return different date formats
 		if date == "Fri May 13 17:52:18 -0500 2011"
 			assert date == "Fri May 13 17:52:18 -0500 2011"
-		elsif date == "2011-05-13 17:52:18 -0500"
-			assert date == "2011-05-13 17:52:18 -0500"
+		#elsif date == "2011-05-13 17:52:18 -0500" #1.8.7
+		#	assert date == "2011-05-13 17:52:18 -0500"
 		end
 	end
-	
+
 	test "Report.scanner_nessus_ratings_text length should be 1523" do
 		Report.company = ""
 		assert Report.scanner_nessus_ratings_text.length == 1523, "GOT #{Report.scanner_nessus_ratings_text.length}"
