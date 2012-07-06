86e796ff8ef6ebab9614f79f0a177a0fa16ba7d2
Jacob Hammack <jacob.hammack@hammackj.com>
2012-07-05 22:03:51 -0500

    Spelling issues

--- lib/risu/templates/pci_compliance.rb
+++ lib/risu/templates/pci_compliance.rb
@@ -1,19 +1,19 @@
 module Risu
 	module Templates
 		class PCICompliance < Risu::Base::TemplateBase
-			
+
 			#
 			#
 			def initialize ()
-				@template_info = 
-				{ 
-					:name => "pci_compliance", 
-					:author => "hammackj", 
-					:version => "0.0.1", 
+				@template_info =
+				{
+					:name => "pci_compliance",
+					:author => "hammackj",
+					:version => "0.0.2",
 					:description => "Generates a PCI Compliance Overview Report"
 				}
 			end
-			
+
 			#
 			#
 			def render(output)
@@ -21,8 +21,8 @@ module Risu
 				output.text "\n"
 
 				output.font_size(22) { output.text Report.title, :align => :center }
-				output.font_size(18) { 
-					output.text "PCI /DSS Complience Overview", :align => :center
+				output.font_size(18) {
+					output.text "PCI /DSS Compliance Overview", :align => :center
 					output.text "\n"
 					output.text "This report was prepared by\n#{Report.author}", :align => :center
 				}
@@ -42,7 +42,7 @@ module Risu
 				output.text "\n\n"
 
 				if @hosts_passed.length > 0
-					output.font_size(20) { 
+					output.font_size(20) {
 						output.fill_color "00FF00"
 						output.text "PCI / DSS Compliant Hosts", :style => :bold
 						output.fill_color "000000"
@@ -50,7 +50,7 @@ module Risu
 
 					output.text "\n"
 
-					@hosts_passed.each do |host|	
+					@hosts_passed.each do |host|
 						output.text "#{host.ip} / #{host.fqdn} - passed\n"
 					end unless @hosts_passed == nil
 
@@ -58,15 +58,15 @@ module Risu
 				end
 
 				if @hosts_failed.length > 0
-					output.font_size(20) { 
+					output.font_size(20) {
 						output.fill_color "FF0000"
-						output.text "Non PCI / DSS Compliant Hosts", :style => :bold 
+						output.text "Non PCI / DSS Compliant Hosts", :style => :bold
 						output.fill_color "000000"
 						}
 
 					output.text "\n"
 
-					@hosts_failed.each do |host|	
+					@hosts_failed.each do |host|
 							host_id = host.id
 							plugin = Plugin.find(:first, :conditions => { :id => 33929 })
 							item = Item.find(:first, :conditions => { :host_id => host_id, :plugin_id => plugin.id })
