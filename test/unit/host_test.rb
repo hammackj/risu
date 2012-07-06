66ad07fee099ebebf0b98212029afdae3f1e87a5
Jacob Hammack <jacob.hammack@hammackj.com>
2012-07-05 21:16:59 -0500

    Added some tests

--- test/unit/host_test.rb
+++ test/unit/host_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
- 
-class HostTest < ActiveSupport::TestCase	
+
+class HostTest < ActiveSupport::TestCase
 	test "returns 10.0.0.1 for Host.ip" do
 		assert_equal Host.first.ip, "10.0.0.1"
 	end
-	
+
 	test "returns sorted ips for Host.sorted" do
 		assert Host.sorted.first.ip == "10.0.0.1" and Host.sorted[1].ip == "10.0.0.2"
 	end
-	
+
 	test "returns 1 for Host.os_windows" do
 		assert_equal Host.os_windows.all.count, 1
 	end
-	
+
 	test "returns 1 for Host.os_windows_xp" do
 		assert_equal Host.os_windows_xp.all.count, 1
 	end
@@ -35,8 +35,8 @@ class HostTest < ActiveSupport::TestCase
 
 	test "returns a graph Host.windows_os_graph" do
 		assert Host.windows_os_graph.class == StringIO
-	end		
-	
+	end
+
 	test "returns 163 for Host.windows_os_graph_text.length" do
 		assert Host.windows_os_graph_text.length == 163, "GOT #{Host.windows_os_graph_text.length}"
 	end
@@ -44,28 +44,32 @@ class HostTest < ActiveSupport::TestCase
 	test "returns 0 for Host.unsupported_os_windows.length" do
 		assert Host.unsupported_os_windows.length == 0, "GOT #{Host.unsupported_os_windows.length}"
 	end
-	
+
 	test "returns 275 for Host.unsupported_os_text.length" do
 		assert Host.unsupported_os_text.length == 275, "GOT #{Host.unsupported_os_text.length}"
 	end
-	
+
 	test "returns 0 for Host.unsupported_os_aix.length" do
 		assert Host.unsupported_os_aix.length == 0, "GOT #{Host.unsupported_os_aix.length}"
 	end
-		
+
 	test "returns 0 for Host.unsupported_os_freebsd.length" do
 		assert Host.unsupported_os_freebsd.length == 0, "GOT #{Host.unsupported_os_freebsd.length}"
 	end
-	
+
 	test "returns 186 for Host.other_os_graph_text.length" do
 		assert Host.other_os_graph_text.length == 186, "GOT #{Host.other_os_graph_text.length}"
 	end
 
 	test "should return 2 for Host.top_n_vulnerable(2)" do
-		assert Host.top_n_vulnerable(2).count == 2, "GOT #{Host.top_n_vulnerable(8).count}"
+		assert Host.top_n_vulnerable(2).count == 2, "GOT #{Host.top_n_vulnerable(2).count}"
 	end
-	
-	test "should return 2 for Host.top_n_vulnerable(10)" do
-		assert Host.top_n_vulnerable(10).count == 2, "GOT #{Host.top_n_vulnerable(8).count}"
+
+	test "should return 2 for Host.unique_hosts_with_critical" do
+		assert Host.unique_hosts_with_critical.count == 2, "GOT #{Host.unique_hosts_with_critical.count}"
+	end
+
+	test "should return 1 for Host.unique_hosts_with_high" do
+		assert Host.unique_hosts_with_high.count == 1, "GOT #{Host.unique_hosts_with_high.count}"
 	end
 end
