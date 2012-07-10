d2a7471e03229edf52c884240596d740f8f894bb
Jacob Hammack <jacob.hammack@hammackj.com>
2012-07-09 22:59:50 -0500

    Whitespace

--- lib/risu/base/templater.rb
+++ lib/risu/base/templater.rb
@@ -1,31 +1,32 @@
 module Risu
 	module Base
-		# Templater class for generating a report from a erb template
+		# Templater class for generating a report from a ERB template
 		#
 		# @author Jacob Hammack
 		class Templater
 			attr_accessor :template, :template_manager, :findings, :output_file
-		
-			# Setups of the Templater class initalizing all of the variables
+
+			# Setups of the Templater class initializing all of the variables
 			#
 			# @return [Templater] New Instance
 			def initialize(template, findings, output, template_manager)
 				@template = template
 				@findings = findings
-				@output_file = output			
+				@output_file = output
 				@template_manager = template_manager
 			end
-		
-			# Generates a report based on the erb template
+
+			# Generates a report based on the ERB template
 			#
 			def generate
 				begin
 					template = @template
 					template_manager = @template_manager
-					
-					Prawn::Document.generate(@output_file, :margin => [75, 50, 75, 50]) do |output|						
+
+					Prawn::Document.generate(@output_file, :margin => [75, 50, 75, 50]) do |output|
 						output.font_size 12
 						t = template_manager.find_template_by_name(template)
+						t = t.class.new
 						t.render(output) unless t == nil
 					end
 				rescue => e
