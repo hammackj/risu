b28b27901b9d6ae51b28a4bb67e604205d477415
Jacob Hammack <jacob.hammack@hammackj.com>
2012-07-20 19:12:57 -0500

    what

--- lib/risu/base.rb
+++ lib/risu/base.rb
@@ -31,5 +31,5 @@ end
 
 require 'risu/base/template_base'
 require 'risu/base/template_manager'
-require 'risu/base/prawn_templater'
+#require 'risu/base/prawn_templater'
 require 'risu/base/templater'
