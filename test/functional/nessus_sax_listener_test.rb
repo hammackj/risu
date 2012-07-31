3b49c746d372da2204958a2cd4a8f9363868cd6e
Jacob Hammack <jacob.hammack@hammackj.com>
2012-07-30 19:55:47 -0500

    Really need to get this test finished

--- test/functional/nessus_sax_listener_test.rb
+++ test/functional/nessus_sax_listener_test.rb
@@ -1,9 +1,9 @@
 # Copyright (c) 2010-2012 Arxopia LLC.
 # All rights reserved.
-
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
 #     * Neither the name of the Arxopia LLC nor the names of its contributors
 #     	may be used to endorse or promote products derived from this software
 #     	without specific prior written permission.
-
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -24,15 +24,24 @@
 #OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 #OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# require 'test_helper'
+ require 'test_helper'
 
-# class NessusSaxListenerTest < ActiveSupport::TestCase
+#"<?xml version="1.0" ?>
+#	<NessusClientData_v2>
+		#<Policy></Policy>
 
-# 	def setup
-# 		@app = Risu::CLI::Application.new
-# 	end
+		#<item></item>
+#
 
-# 	#test "" do
-# 	#end
+#
 
-# end
+ class NessusSaxListenerTest < ActiveSupport::TestCase
+
+ 	def setup
+ 		@app = Risu::CLI::Application.new
+ 	end
+
+ 	#test "" do
+ 	#end
+
+ end
