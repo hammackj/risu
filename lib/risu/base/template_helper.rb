3239b66e8d114a0700e3177044abe4b0e2860f5f
Jacob Hammack <jacob.hammack@hammackj.com>
2012-09-01 01:47:22 -0500

    Added a template_helper mixin to hold methods that are used in every plugin

---
+++ lib/risu/base/template_helper.rb
@@ -0,0 +1,98 @@
+# Copyright (c) 2012 Arxopia LLC.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Arxopia LLC nor the names of its contributors
+#     	may be used to endorse or promote products derived from this software
+#     	without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL ARXOPIA LLC BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+#OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+#OF THE POSSIBILITY OF SUCH DAMAGE.
+
+module Risu
+	module Templates
+		module TemplateHelper
+
+			#
+			def report_title title
+				@output.font_size(24) do
+					@output.text title, :align => :center
+					@output.text "\n"
+				end
+			end
+
+			#
+			def report_subtitle title
+				@output.font_size(18) do
+					@output.text title, :align => :center
+					@output.text "\n"
+				end
+			end
+
+			#
+			def report_author author
+				@output.font_size(14) do
+					@output.text author, :align => :center
+					@output.text "\n"
+				end
+			end
+
+			#
+			def heading1 title
+				@output.font_size(24) do
+					@output.text title, :style => :bold
+				end
+			end
+
+			#
+			def heading2 title
+				@output.font_size(18) do
+					@output.text title, :style => :bold
+				end
+			end
+
+			#
+			def heading3 title
+				@output.font_size(14) do
+					@output.text title, :style => :bold
+				end
+			end
+
+			#
+			def heading4 title
+				@output.font_size(12) do
+					@output.text title, :style => :bold
+				end
+			end
+
+			#
+			def heading5 title
+				@output.font_size(10) do
+					@output.text title, :style => :bold
+				end
+			end
+
+			#
+			def heading6 title
+				@output.font_size(8) do
+					@output.text title, :style => :bold
+				end
+			end
+		end
+	end
+end
