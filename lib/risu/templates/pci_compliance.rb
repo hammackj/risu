207190c3772f3913cc5dcec1ee83146b948df846
Jacob Hammack <jacob.hammack@hammackj.com>
2013-02-17 06:43:18 -0600

    Updated the template to support the new Templatehelper mixin

--- lib/risu/templates/pci_compliance.rb
+++ lib/risu/templates/pci_compliance.rb
@@ -27,83 +27,77 @@
 module Risu
 	module Templates
 		class PCICompliance < Risu::Base::TemplateBase
+			include TemplateHelper
 
-			#
-			#
 			def initialize ()
 				@template_info =
 				{
 					:name => "pci_compliance",
 					:author => "hammackj",
-					:version => "0.0.3",
+					:version => "0.0.4",
 					:description => "Generates a PCI Compliance Overview Report"
 				}
 			end
 
-			#
-			#
 			def render(output)
-				output.text Report.classification.upcase, :align => :center
-				output.text "\n"
+				report_text Report.classification.upcase, :align => :center
+				report_text "\n"
 
-				output.font_size(22) { output.text Report.title, :align => :center }
-				output.font_size(18) {
-					output.text "PCI / DSS Compliance Overview", :align => :center
-					output.text "\n"
-					output.text "This report was prepared by\n#{Report.author}", :align => :center
-				}
+				report_title Report.title
+				report_subtitle "PCI / DSS Compliance Overview"
+				report_author "This report was prepared by\n#{Report.author}"
 
-				output.text "\n\n\n"
+				report_text "\n\n\n"
 
 				@hosts_count = Host.find(:all, :conditions => ["pci_dss_compliance is not null"]).count
 				@hosts_passed = Host.find(:all, :conditions => ["pci_dss_compliance like 'passed'"])
 				@hosts_failed = Host.find(:all, :conditions => ["pci_dss_compliance like 'failed'"])
 
-				output.font_size(20) {
+				output.font_size(20) do
 					output.text "Summary\n", :style => :bold
-				}
+				end
 
-				output.text "Of #{@hosts_count} total hosts, #{@hosts_passed.count} passed and #{@hosts_failed.count} failed."
+				report_text "Of #{@hosts_count} total hosts, #{@hosts_passed.count} passed and #{@hosts_failed.count} failed."
 
-				output.text "\n\n"
+				report_text "\n\n"
 
 				if @hosts_passed.length > 0
-					output.font_size(20) {
+					output.font_size(20) do
 						output.fill_color "00FF00"
 						output.text "PCI / DSS Compliant Hosts", :style => :bold
 						output.fill_color "000000"
-						}
+					end
 
 					output.text "\n"
 
 					@hosts_passed.each do |host|
-						output.text "#{host.ip} / #{host.fqdn} - passed\n"
+						report_text "#{host.ip} / #{host.fqdn} - passed\n"
 					end unless @hosts_passed == nil
 
 					output.start_new_page
 				end
 
 				if @hosts_failed.length > 0
-					output.font_size(20) {
+					output.font_size(20) do
 						output.fill_color "FF0000"
 						output.text "Non PCI / DSS Compliant Hosts", :style => :bold
 						output.fill_color "000000"
-						}
+					end
 
-					output.text "\n"
+					report_text "\n"
 
 					@hosts_failed.each do |host|
-							host_id = host.id
-							plugin = Plugin.find(:first, :conditions => { :id => 33929 })
-							item = Item.find(:first, :conditions => { :host_id => host_id, :plugin_id => plugin.id })
+						host_id = host.id
+						plugin = Plugin.find(:first, :conditions => { :id => 33929 })
+						item = Item.find(:first, :conditions => { :host_id => host_id, :plugin_id => plugin.id })
 
-							output.text "#{host.ip} / #{host.fqdn} - failed\n", :style => :bold
-							output.text "Description:\n", :style => :bold
-							output.text "#{plugin.description}\n"
-							output.text "Plugin Output:\n", :style => :bold
-							output.text "#{item.plugin_output}\n"
+						output.text "#{host.ip} / #{host.fqdn} - failed\n", :style => :bold
+						output.text "Description:\n", :style => :bold
+						output.text "#{plugin.description}\n"
+						output.text "Plugin Output:\n", :style => :bold
+						output.text "#{item.plugin_output}\n"
 
-							output.text "\n"
+						report_text "\n"
 
 					end unless @hosts_failed == nil
 
