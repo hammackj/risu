c1d84950c787b9154d5776c5fc9b1a4442c8af12
Jacob Hammack <jacob.hammack@hammackj.com>
2013-02-18 11:19:26 -0600

    Updated template to use new TemplateHelper

--- lib/risu/templates/cover_sheet.rb
+++ lib/risu/templates/cover_sheet.rb
@@ -27,66 +27,62 @@
 module Risu
 	module Templates
 		class CoverSheet < Risu::Base::TemplateBase
+			include TemplateHelper
 
-			#
-			#
 			def initialize ()
 				@template_info =
 				{
 					:name => "cover_sheet",
 					:author => "hammackj",
-					:version => "0.0.2",
+					:version => "0.0.3",
 					:description => "Generates a coversheet with a logo (Example Template)"
 				}
 			end
 
-			#
-			#
 			def render(output)
 				output.image "#{File.expand_path(File.dirname(__FILE__))}/data/nessuslogo.jpg", :scale => 0.2, :position => :left, :vposition => :top
 
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
 
-				output.font_size(24) do
-					output.text Report.title, :align => :center
-				end
+				text Report.classification.upcase, :align => :center
+				text "\n"
 
-				output.font_size(18) do
-				    output.text "Coversheet Example", :align => :center
-				    output.text "\n"
-				    output.text "This report was prepared by\n#{Report.author}", :align => :center
-				end
+				report_title Report.title
+				report_subtitle "Coversheet Example"
+				report_author "This report was prepared by\n#{Report.author}"
 
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
-				output.text "\n"
+				text "\n\n\n"
 
-				output.text "Nessus is a Registered Trademark of Tenable Network Security, Inc."
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+				text "\n"
+
+				text "Nessus is a Registered Trademark of Tenable Network Security, Inc."
 			end
 		end
 	end
