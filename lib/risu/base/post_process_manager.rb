35e633c9b16f6a1f494da306cbf9676f97f64067
Jacob Hammack <jacob.hammack@hammackj.com>
2015-11-30 21:26:04 -0600

    Doc tweaks

--- lib/risu/base/post_process_manager.rb
+++ lib/risu/base/post_process_manager.rb
@@ -92,7 +92,7 @@ module Risu
 
 			# Validates that a template is a valid template
 			#
-			# @todo look at refactoring this to valid?(template)
+			# @TODO look at refactoring this to valid?(template)
 			#
 			# @param template The template to validate
 			#
