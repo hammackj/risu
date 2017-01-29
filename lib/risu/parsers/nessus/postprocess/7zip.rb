2ca94ac25c9a04f523a7e3f65d31499aae7797ea
Jacob Hammack <jacob.hammack@hammackj.com>
2017-01-29 15:58:54 -0600

    Added 7zip post processing

---
+++ lib/risu/parsers/nessus/postprocess/7zip.rb
@@ -0,0 +1,46 @@
+# Copyright (c) 2010-2016 Arxopia LLC.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+module Risu
+	module Parsers
+		module Nessus
+			module PostProcess
+				class SevenZipPatchRollup < Risu::Base::PostProcessBase
+
+					#
+					def initialize
+						@info =
+						{
+							:description => "7-Zip Patch Rollup",
+							:plugin_id => -99954,
+							:plugin_name => "Update to the latest 7-Zip",
+							:item_name => "Update to the latest 7-Zip",
+							:plugin_ids => [
+								91230,
+								
+							]
+						}
+					end
+				end
+			end
+		end
+	end
+end
