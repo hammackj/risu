8bbe00f7eb19fead8a9c6afeea236b8613c8aa15
Jacob Hammack <jacob.hammack@hammackj.com>
2018-02-04 18:27:06 -0600

    Updated Foxit Phantom Post Processing

--- lib/risu/parsers/nessus/postprocess/foxit_phantom_pdf.rb
+++ lib/risu/parsers/nessus/postprocess/foxit_phantom_pdf.rb
@@ -31,11 +31,14 @@ module Risu
 						{
 							:description => "Foxit PhantomPDF Patch Rollup",
 							:plugin_id => -99956,
-							:plugin_name => "Update to the latest Foxit PhantomPDF",
+							:plugin_name => "Missing the latest Foxit PhantomPDF Patches",
 							:item_name => "Update to the latest Foxit PhantomPDF",
 							:plugin_ids => [
 								86697,
-								90566
+								90566,
+								102682, #Foxit PhantomPDF < 7.3.15 Multiple Vulnerabilities - 102682
+								102858, #Foxit PhantomPDF < 8.3.2 Multiple Vulnerabilities - 102858
+								101523, #Foxit PhantomPDF < 8.3.1 Multiple Vulnerabilities - 101523
 							]
 						}
 					end
