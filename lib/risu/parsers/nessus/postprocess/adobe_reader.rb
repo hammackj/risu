b1d8e86c505f99eef9553b0d218a4fadfd416c33
Jacob Hammack <jacob.hammack@hammackj.com>
2018-02-04 18:24:08 -0600

    Updated Adobe Product Post Processing

--- lib/risu/parsers/nessus/postprocess/adobe_reader.rb
+++ lib/risu/parsers/nessus/postprocess/adobe_reader.rb
@@ -31,67 +31,59 @@ module Risu
 						{
 							:description => "Adobe Reader Patch Rollup",
 							:plugin_id => -99998,
-							:plugin_name => "Missing the latest Adobe Reader",
+							:plugin_name => "Missing the latest Adobe Reader Patches",
 							:item_name => "Update to the latest Adobe Reader",
 							:plugin_ids => [
-								30200,
-								33256,
-								34695,
-								35821,
-								38746,
-								39355,
-								42120,
-								43876,
-								44644,
-								45505,
-								47165,
-								48375,
-								49173,
-								50614,
-								51925,
-								55144,
-								56198,
-								56213,
-								66517,
-								66542,
-								74012,
-								58683,
-								61562,
-								63454,
-								64786,
-								66410,
-								69846,
-								71947,
-								57043,
-								57484,
-								24002,
-								23776,
-								23975,
-								52672,
-								53451,
-								21698,
-								77712,
-								79856,
-								77175,
-								83471,
-								40494,
-								27584,
-								86403,
-								87918,
-								89831,
-								70343,
-								91097,
-								92035,
-								94072,
-								96453,
-								99374,
-								100756,
-								101362,
-								102428,
-								
-
-
-
+								71947, #Adobe Reader < 10.1.9 / 11.0.6 Multiple Vulnerabilities (APSB14-01)
+								30200, #Adobe Reader < 7.1.0 / 8.1.2 Multiple Vulnerabilities
+								33256, #Adobe Reader < 7.1.0 / 8.1.2 SU1 Unspecified JavaScript Method Handling Arbitrary Code Execution
+								34695, #Adobe Reader < 8.1.3 / 9.0 Multiple Vulnerabilities
+								35821, #Adobe Reader < 9.1 / 8.1.4 / 7.1.1 Multiple Vulnerabilities
+								38746, #Adobe Reader getAnnots() JavaScript Method PDF Handling Memory Corruption (APSB09-06)
+								39355, #Adobe Reader < 9.1.2 / 8.1.6 / 7.1.3 Multiple Vulnerabilities
+								42120, #Adobe Reader < 9.2 / 8.1.7 / 7.1.4  Multiple Vulnerabilities (APSB09-15)
+								43876, #Adobe Reader < 9.3 / 8.2  Multiple Vulnerabilities (APSB10-02)
+								44644, #Adobe Reader < 9.3.1 / 8.2.1  Multiple Vulnerabilities (APSB10-07)
+								45505, #Adobe Reader < 9.3.2 / 8.2.2  Multiple Vulnerabilities (APSB10-09)
+								47165, #Adobe Reader < 9.3.3 / 8.2.3  Multiple Vulnerabilities (APSB10-15)
+								48375, #Adobe Reader < 9.3.4 / 8.2.4  Multiple Vulnerabilities (APSB10-17)
+								49173, #Adobe Reader < 9.4 / 8.2.5 Multiple Vulnerabilities (APSB10-21)
+								50614, #Adobe Reader < 9.4.1 Multiple Vulnerabilities (APSB10-28)
+								51925, #Adobe Reader < 10.0.1 / 9.4.2 / 8.2.6 Multiple Vulnerabilities (APSB11-03)
+								55144, #Adobe Reader < 10.1 / 9.4.5 / 8.3 Multiple Vulnerabilities (APSB11-16)
+								56198, #Adobe Reader < 10.1.1 / 9.4.6 / 8.3.1 Multiple Vulnerabilities (APSB11-21, APSB11-24)
+								74012, #Adobe Reader < 10.1.10 / 11.0.07 Multiple Vulnerabilities (APSB14-15)
+								58683, #Adobe Reader < 10.1.3 / 9.5.1 Multiple Vulnerabilities (APSB12-08)
+								61562, #Adobe Reader < 10.1.4 / 9.5.2 Multiple Vulnerabilities (APSB12-16)
+								63454, #Adobe Reader < 11.0.1 / 10.1.5 / 9.5.3 Multiple Vulnerabilities (APSB13-02)
+								64786, #Adobe Reader < 11.0.2 / 10.1.6 / 9.5.4 Multiple Vulnerabilities (APSB13-07)
+								66410, #Adobe Reader < 11.0.3 / 10.1.7 / 9.5.5 Multiple Vulnerabilities (APSB13-15)
+								69846, #Adobe Reader < 11.0.4 / 10.1.8 Multiple Vulnerabilities (APSB13-22)
+								71947, #Adobe Reader < 10.1.9 / 11.0.6 Multiple Vulnerabilities (APSB14-01)
+								57043, #Adobe Reader < 9.4.7 Multiple Memory Corruption Vulnerabilities (APSB11-30)
+								57484, #Adobe Reader < 10.1.2 / 9.5 Multiple Vulnerabilities (APSB12-01)
+								24002, #Adobe Reader < 6.0.6 / 7.0.9 Multiple Vulnerabilities
+								23776, #Adobe Reader < 8.0 AcroPDF ActiveX Control Multiple Vulnerabilities
+								23975, #Adobe PDF Plug-In < 8.0 / 7.0.9 / 6.0.6 Multiple Vulnerabilities (APSB07-01)
+								52672, #Adobe Reader 9.x / 10.x Unspecified Memory Corruption (APSB11-06)
+								53451, #Adobe Reader 9.x / 10.x Multiple Vulnerabilities (APSB11-08)
+								21698, #Adobe Reader < 7.0.8 Multiple Unspecified Vulnerabilities
+								77712, #Adobe Reader < 10.1.12 / 11.0.09 Multiple Vulnerabilities (APSB14-20)
+								79856, #Adobe Reader < 10.1.13 / 11.0.10 Multiple Vulnerabilities (APSB14-28)
+								77175, #Adobe Reader < 10.1.11 / 11.0.08 Sandbox Bypass (APSB14-19)
+								83471, #Adobe Reader < 10.1.14 / 11.0.11 Multiple Vulnerabilities (APSB15-10)
+								40494, #Adobe Reader < 9.1.3 Flash Handling Unspecified Arbitrary Code Execution (APSB09-10)
+								27584, #Adobe Reader < 8.1.1 Crafted PDF File Arbitrary Code Execution
+								86403, #Adobe Reader <= 10.1.15 / 11.0.12 / 2015.006.30060 / 2015.008.20082 Multiple Vulnerabilities (APSB15-24)
+								87918, #Adobe Reader < 11.0.14 / 15.006.30119 / 15.010.20056 Multiple Vulnerabilities (APSB16-02)
+								89831, #Adobe Reader < 11.0.15 / 15.006.30121 / 15.010.20060 Multiple Vulnerabilities (APSB16-09)
+								70343, #Adobe Reader 11.0.4 Crafted PDF File Handling JavaScript Scheme URI Execution (APSB13-25)
+								91097, #Adobe Reader < 11.0.16 / 15.006.30172 / 15.016.20039 Multiple Vulnerabilities (APSB16-14)
+								92035, #Adobe Reader < 11.0.17 / 15.006.30198 / 15.017.20050 Multiple Vulnerabilities (APSB16-26)
+								94072, #Adobe Reader < 11.0.18 / 15.006.30243 / 15.020.20039 Multiple Vulnerabilities (APSB16-33)
+								96453, #Adobe Reader < 11.0.19 / 15.006.30279 / 15.023.20053 Multiple Vulnerabilities (APSB17-01)
+								99374, #Adobe Reader < 11.0.20 / 2015.006.30306 / 2017.009.20044 Multiple Vulnerabilities (APSB17-11)
+								102428, #Adobe Reader < 11.0.21 / 2015.006.30355 / 2017.011.30066 / 2017.012.20098 Multiple Vulnerabilities (APSB17-24)
 							]
 						}
 					end
--- lib/risu/parsers/nessus/postprocess/flash_player.rb
+++ lib/risu/parsers/nessus/postprocess/flash_player.rb
@@ -31,112 +31,112 @@ module Risu
 						{
 							:description => "Flash Player Patch Rollup",
 							:plugin_id => -99997,
-							:plugin_name => "Missing the latest Flash Player",
+							:plugin_name => "Missing the latest Flash Player Patches",
 							:item_name => "Update to the latest Flash Player",
 							:plugin_ids => [
-								11323,
-								11952,
-								20158,
-								21079,
-								22056,
-								23869,
-								25694,
-								29741,
-								31799,
-								34741,
-								35742,
-								40434,
-								43068,
-								44596,
-								46859,
-								48300,
-								49307,
-								50493,
-								51926,
-								52673,
-								53472,
-								54299,
-								54972,
-								55140,
-								55803,
-								56259,
-								56874,
-								58001,
-								58207,
-								58538,
-								58994,
-								59196,
-								59426,
-								61550,
-								61622,
-								62480,
-								62836,
-								63242,
-								63450,
-								64506,
-								64584,
-								64916,
-								65219,
-								65910,
-								66445,
-								66872,
-								67225,
-								69866,
-								70858,
-								71351,
-								71951,
-								72284,
-								72606,
-								72937,
-								73433,
-								73740,
-								73994,
-								74431,
-								76413,
-								77172,
-								77577,
-								78441,
-								79140,
-								79442,
-								79835,
-								80484,
-								80946,
-								80998,
-								81127,
-								81819,
-								82781,
-								83365,
-								84048,
-								84365,
-								84642,
-								84730,
-								85326,
-								86060,
-								86369,
-								86423,
-								86851,
-								87244,
-								87657,
-								88639,
-								89834,
-								90425,
-								91163,
-								91670,
-								92012,
-								93461,
-								93960,
-								94334,
-								94628,
-								95762,
-								96388,
-								97142,
-								97727,
-								99283,
-								100052,
-								102262,
-								
-
-
+								100756, #Adobe Flash Player <= 25.0.0.171 Multiple Vulnerabilities (APSB17-17)
+								11323, #Flash Player < 6.0.79.0 Multiple Unspecified Overflows
+								11952, #Flash Player < 7.0.19.0 Predictable Data Location Weakness
+								20158, #Flash Player < 7.0.60.0 / 8.0.22.0 Multiple Vulnerabilities
+								21079, #Flash Player swf Processing Multiple Unspecified Code Execution (APSB06-03)
+								22056, #Flash Player Multiple Vulnerabilities (APSB06-11)
+								23869, #Flash Player HTTP Header CRLF Injection (APSB06-18)
+								25694, #Flash Player Multiple Vulnerabilities (APSB07-12)
+								29741, #Flash Player < 7.0.73.0 / 9.0.115.0 Multiple Vulnerabilities (APSB07-20)
+								31799, #Flash Player < 8.0.42.0 / 9.0.124.0 Multiple Vulnerabilities (APSB08-11)
+								34741, #Flash Player < 9.0.151.0 / 10.0.12.36 Multiple Vulnerabilities (APSB08-18 / APSB08-20 / APSB08-22)
+								35742, #Flash Player 9.0.159.0 / 10.0.22.87 Multiple Vulnerabilities (APSB09-01)
+								40434, #Flash Player < 9.0.246.0 / 10.0.32.18 Multiple Vulnerabilities (APSB09-10)
+								43068, #Flash Player < 9.0.260 / 10.0.42.34 Multiple Vulnerabilities (APSB09-19)
+								44596, #Flash Player < 10.0.45.2 Multiple Vulnerabilities (APSB10-06)
+								46859, #Flash Player < 9.0.277.0 / 10.1.53.63 Multiple Vulnerabilities (ASPB10-14)
+								48300, #Flash Player < 9.0.280 / 10.1.82.76 Multiple Vulnerabilities (APSB10-16)
+								49307, #Flash Player < 9.0.283 / 10.1.85.3 Unspecified Code Execution (APSB10-22)
+								50493, #Flash Player < 9.0.289 / 10.1.102.64 Multiple Vulnerabilities (APSB10-26)
+								51926, #Flash Player < 10.2.152.26 Multiple Vulnerabilities (APSB11-02)
+								52673, #Flash Player < 10.2.153.1 Unspecified Memory Corruption (APSB11-05)
+								53472, #Flash Player < 10.2.159.1 ActionScript Predefined Class Prototype Addition Remote Code Execution (APSB11-07)
+								54299, #Flash Player < 10.3.181.14 Multiple Vulnerabilities (APSB11-12)
+								54972, #Flash Player < 10.3.181.22 XSS (APSB11-13)
+								55140, #Flash Player < 10.3.181.26 Multiple Vulnerabilities (APSB11-18)
+								55803, #Flash Player <= 10.3.181.36 Multiple Vulnerabilities (APSB11-21)
+								56259, #Flash Player <= 10.3.183.7 Multiple Vulnerabilities (APSB11-26)
+								56874, #Flash Player <= 10.3.183.10 / 11.0.1.152 Multiple Vulnerabilities (APSB11-28)
+								58001, #Flash Player <= 10.3.183.14 / 11.1.102.55 Multiple Vulnerabilities (APSB12-03)
+								58207, #Flash Player <= 10.3.183.15 / 11.1.102.62 Multiple Vulnerabilities (APSB12-05)
+								58538, #Flash Player <= 10.3.183.16 / 11.1.102.63 Multiple Memory Corruption Vulnerabilities (APSB12-07)
+								58994, #Flash Player <= 10.3.183.18 / 11.2.202.233 Object Confusion Vulnerability (APSB12-09)
+								59426, #Flash Player <= 10.3.183.19 / 11.3.300.256 Multiple Vulnerabilities (APSB12-14)
+								61550, #Flash Player <= 11.3.300.270 Code Execution (APSB12-18)
+								61622, #Flash Player <= 10.3.183.22 / 11.4.402.264 Multiple Vulnerabilities (APSB12-19)
+								62480, #Flash Player <= 10.3.183.23 / 11.4.402.278 Multiple Vulnerabilities (APSB12-22)
+								62836, #Flash Player <= 10.3.183.29 / 11.4.402.287 Multiple Vulnerabilities (APSB12-24)
+								63242, #Flash Player <= 10.3.183.43 / 11.5.502.110 Multiple Vulnerabilities (APSB12-27)
+								63450, #Flash Player <= 10.3.183.48 / 11.5.502.135 Buffer Overflow (APSB13-01)
+								64506, #Flash Player <= 10.3.183.50 / 11.5.502.146 Multiple Vulnerabilities (APSB13-04)
+								64584, #Flash Player <= 10.3.183.51 / 11.5.502.149 Multiple Vulnerabilities (APSB13-05)
+								64916, #Flash Player <= 10.3.183.63 / 11.6.602.168 Multiple Vulnerabilities (APSB13-08)
+								65219, #Flash Player <= 10.3.183.67 / 11.6.602.171 Multiple Vulnerabilities (APSB13-09)
+								65910, #Flash Player <= 10.3.183.68 / 11.6.602.180 Multiple Vulnerabilities (APSB13-11)
+								66445, #Flash Player <= 10.3.183.75 / 11.7.700.169 Multiple Vulnerabilities (APSB13-14)
+								66872, #Flash Player <= 10.3.183.86 / 11.7.700.202 Memory Corruption (APSB13-16)
+								67225, #Flash Player <= 10.3.183.90 / 11.7.700.224 Multiple Vulnerabilities (APSB13-17)
+								69866, #Flash Player <= 11.7.700.232 / 11.8.800.94 Memory Corruptions (APSB13-21)
+								70858, #Flash Player <= 11.7.700.242 / 11.9.900.117 Memory Corruptions (APSB13-26)
+								71351, #Flash Player <= 11.7.700.252 / 11.9.900.152 Multiple Vulnerabilities (APSB13-28)
+								71951, #Flash Player <= 11.7.700.257 / 11.9.900.170 Multiple Vulnerabilities (APSB14-02)
+								72284, #Flash Player <= 11.7.700.260 / 12.0.0.43 Unspecified Remote Code Execution (APSB14-04)
+								72606, #Flash Player <= 11.7.700.261 / 12.0.0.44 Multiple Vulnerabilities (APSB14-07)
+								72937, #Flash Player <= 11.7.700.269 / 12.0.0.70 Multiple Vulnerabilities (APSB14-08)
+								73433, #Flash Player <= 11.7.700.272 / 12.0.0.77 Multiple Vulnerabilities (APSB14-09)
+								73740, #Flash Player <= 11.7.700.275 / 13.0.0.182 Pixel Bender Component Buffer Overflow (APSB14-13)
+								73994, #Flash Player <= 13.0.0.206 Multiple Vulnerabilities (APSB14-14)
+								74431, #Flash Player <= 13.0.0.214 Multiple Vulnerabilities (APSB14-16)
+								76413, #Flash Player <= 14.0.0.125 Multiple Vulnerabilities (APSB14-17)
+								77172, #Flash Player <= 14.0.0.145 Multiple Vulnerabilities (APSB14-18)
+								77577, #Flash Player <= 14.0.0.179 Multiple Vulnerabilities (APSB14-21)
+								78441, #Flash Player <= 15.0.0.167 Multiple Vulnerabilities (APSB14-22)
+								79140, #Flash Player <= 15.0.0.189 Multiple Vulnerabilities (APSB14-24)
+								79442, #Flash Player <= 15.0.0.223 Dereferenced Memory Pointer RCE (APSB14-26)
+								79835, #Flash Player <= 15.0.0.239 Multiple Vulnerabilities (APSB14-27)
+								80484, #Flash Player <= 16.0.0.235 Multiple Vulnerabilities (APSB15-01)
+								80946, #Flash Player <= 16.0.0.257 Information Disclosure (APSB15-02)
+								80998, #Flash Player <= 16.0.0.287 Unspecified Code Execution (APSA15-01 / APSB15-03)
+								81127, #Flash Player <= 16.0.0.296 Unspecified Code Execution (APSA15-02 / APSB15-04)
+								81819, #Flash Player <= 16.0.0.305 Multiple Vulnerabilities (APSB15-05)
+								82781, #Adobe Flash Player <= 17.0.0.134 Multiple Vulnerabilities (APSB15-06)
+								83365, #Adobe Flash Player <= 17.0.0.169 Multiple Vulnerabilities (APSB15-09)
+								84048, #Adobe Flash Player <= 17.0.0.188 Multiple Vulnerabilities (APSB15-11)
+								84365, #Adobe Flash Player <= 18.0.0.161 RCE (APSB15-14)
+								84642, #Adobe Flash Player <= 18.0.0.194 Multiple Vulnerabilities (APSB15-16)
+								84730, #Adobe Flash Player <= 18.0.0.203 Multiple RCE Vulnerabilities (APSB15-18)
+								85326, #Adobe Flash Player <= 18.0.0.209 Multiple Vulnerabilities (APSB15-19)
+								86060, #Adobe Flash Player <= 18.0.0.232 Multiple Vulnerabilities (APSB15-23)
+								86369, #Adobe Flash Player <= 19.0.0.185 Multiple Vulnerabilities (APSB15-25)
+								86423, #Adobe Flash Player <= 19.0.0.207 Vulnerability (APSB15-27)
+								86851, #Adobe Flash Player <= 19.0.0.226 Multiple Vulnerabilities (APSB15-28)
+								87244, #Adobe Flash Player <= 19.0.0.245 Multiple Vulnerabilities (APSB15-32)
+								87657, #Adobe Flash Player <= 20.0.0.235 Multiple Vulnerabilities (APSB16-01)
+								88639, #Adobe Flash Player <= 20.0.0.286 Multiple Vulnerabilities (APSB16-04)
+								89834, #Adobe Flash Player <= 20.0.0.306 Multiple Vulnerabilities (APSB16-08)
+								90425, #Adobe Flash Player <= 21.0.0.197 Multiple Vulnerabilities (APSB16-10)
+								91163, #Adobe Flash Player <= 21.0.0.226 Multiple Vulnerabilities (APSB16-15)
+								91670, #Adobe Flash Player <= 21.0.0.242 Multiple Vulnerabilities (APSB16-18)
+								92012, #Adobe Flash Player <= 22.0.0.192 Multiple Vulnerabilities (APSB16-25)
+								93461, #Adobe Flash Player <= 22.0.0.211 Multiple Vulnerabilities (APSB16-29)
+								93960, #Adobe Flash Player <= 23.0.0.162 Multiple Vulnerabilities (APSB16-32)
+								94334, #Adobe Flash Player <= 23.0.0.185 Arbitrary Code Execution (APSB16-36)
+								94628, #Adobe Flash Player <= 23.0.0.205 Multiple Vulnerabilities (APSB16-37)
+								95762, #Adobe Flash Player <= 23.0.0.207 Multiple Vulnerabilities (APSB16-39)
+								96388, #Adobe Flash Player <= 24.0.0.186 Multiple Vulnerabilities (APSB17-02)
+								97142, #Adobe Flash Player <= 24.0.0.194 Multiple Vulnerabilities (APSB17-04)
+								97727, #Adobe Flash Player <= 24.0.0.221 Multiple Vulnerabilities (APSB17-07)
+								99283, #Adobe Flash Player <= 25.0.0.127 Multiple Vulnerabilities (APSB17-10)
+								100052, #Adobe Flash Player <= 25.0.0.148 Multiple Vulnerabilities (APSB17-15)
+								102262, #Adobe Flash Player <= 26.0.0.137 Multiple Vulnerabilities (APSB17-23)
+								101362, #Adobe Flash Player <= 26.0.0.131 Multiple Vulnerabilities (APSB17-21)
+								103124, #Adobe Flash Player <= 26.0.0.151 Multiple Vulnerabilities (APSB17-28)
+								103922, #Adobe Flash Player <= 27.0.0.159 Type Confusion Vulnerability (APSB17-32)								
 							]
 						}
 					end
