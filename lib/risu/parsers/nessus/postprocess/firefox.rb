7acb86b763815399dc80c0fb6f6ea968dfee124b
Jacob Hammack <jacob.hammack@hammackj.com>
2018-02-04 18:26:49 -0600

    Updated Firefox Post Processing

--- lib/risu/parsers/nessus/postprocess/firefox.rb
+++ lib/risu/parsers/nessus/postprocess/firefox.rb
@@ -31,78 +31,78 @@ module Risu
 						{
 							:description => "Firefox Patch Rollup",
 							:plugin_id => -99972,
-							:plugin_name => "Update to the latest Firefox",
+							:plugin_name => "Missing the latest Firefox Patches",
 							:item_name => "Update to the latest Firefox",
 							:plugin_ids => [
-                73099,
-                73769,
-                74440,
-                76763,
-                77500,
-                77906,
-                78473,
-                79665,
-                80523,
-                81521,
-                82040,
-                82041,
-                82503,
-                82998,
-                83439,
-                84581,
-								82583,
-								85386,
-								62998,
-								63551,
-								64723,
-								65131,
-								65806,
-								66480,
-								66993,
-								69269,
-								69993,
-								70716,
-								70949,
-								71347,
-								72331,
-								85275,
-								85689,
-								86071,
-								86764,
-								87476,
-								86418,
-								88461,
-								89875,
-								90793,
-								91547,
-								88754,
-								92755,
-								93662,
-								94960,
-								95475,
-								95886,
-								96776,
-								97639,
-								99125,
-								99632,
-								100127,
-								55901,
-								56334,
-								56750,
-								57768,
-								57769,
-								58006,
-								58349,
-								58898,
-								59407,
-								60043,
-								61715,
-								62580,
-								62589,
-								94232,
-								
-
-
+								73099, #Firefox < 28.0 Multiple Vulnerabilities
+								73769, #Firefox < 29.0 Multiple Vulnerabilities
+								74440, #Firefox < 30.0 Multiple Vulnerabilities
+								76763, #Firefox < 31.0 Multiple Vulnerabilities
+								77500, #Firefox < 32.0 Multiple Vulnerabilities
+								77906, #Firefox < 32.0.3 NSS Signature Verification Vulnerability
+								78473, #Firefox < 33.0 Multiple Vulnerabilities
+								79665, #Firefox < 34.0 Multiple Vulnerabilities
+								80523, #Firefox < 35 Multiple Vulnerabilities
+								81521, #Firefox < 36 Multiple Vulnerabilities
+								82040, #Firefox < 36.0.3 JIT Code Execution
+								82041, #Firefox < 36.0.4 SVG Bypass Privilege Escalation
+								82503, #Firefox < 37.0 Multiple Vulnerabilities
+								82998, #Firefox < 37.0.2 Failed Plugin Memory Corruption
+								83439, #Firefox < 38.0 Multiple Vulnerabilities
+								84581, #Firefox < 39.0 Multiple Vulnerabilities (Logjam)
+								82583, #Firefox < 37.0.1 HTTP/2 Alt-Svc Header Certificate Verification Bypass
+								85386, #Firefox < 40 Multiple Vulnerabilities
+								62998, #Firefox < 17.0 Multiple Vulnerabilities
+								63551, #Firefox < 18.0 Multiple Vulnerabilities
+								64723, #Firefox < 19.0 Multiple Vulnerabilities
+								65131, #Firefox < 19.0.2 nsHTMLEditor Use-After-Free
+								65806, #Firefox < 20 Multiple Vulnerabilities
+								66480, #Firefox < 21.0 Multiple Vulnerabilities
+								66993, #Firefox < 22.0 Multiple Vulnerabilities
+								69269, #Firefox < 23.0 Multiple Vulnerabilities
+								69993, #Firefox < 24.0 Multiple Vulnerabilities
+								70716, #Firefox < 25.0 Multiple Vulnerabilities
+								70949, #Firefox < 25.0.1 NSS and NSPR Multiple Vulnerabilities
+								71347, #Firefox < 26.0 Multiple Vulnerabilities
+								72331, #Firefox < 27.0 Multiple Vulnerabilities
+								85275, #Firefox < 39.0.3 PDF Reader Arbitrary File Access
+								85689, #Firefox < 40.0.3 Multiple Vulnerabilities
+								86071, #Firefox < 41 Multiple Vulnerabilities
+								86764, #Firefox < 42 Multiple Vulnerabilities
+								87476, #Firefox < 43 Multiple Vulnerabilities
+								86418, #Firefox < 41.0.2 'fetch' API Cross-Origin Bypass
+								88461, #Firefox < 44 Multiple Vulnerabilities
+								89875, #Firefox < 45 Multiple Vulnerabilities
+								90793, #Firefox < 46 Multiple Vulnerabilities
+								91547, #Firefox < 47 Multiple Vulnerabilities
+								88754, #Firefox < 44.0.2 Service Workers Security Bypass
+								92755, #Firefox < 48 Multiple Vulnerabilities
+								93662, #Mozilla Firefox < 49.0 Multiple Vulnerabilities
+								94960, #Mozilla Firefox < 50.0 Multiple Vulnerabilities
+								95475, #Mozilla Firefox < 50.0.2 nsSMILTimeContainer.cpp SVG Animation RCE
+								95886, #Mozilla Firefox < 50.1 Multiple Vulnerabilities
+								96776, #Mozilla Firefox < 51.0 Multiple Vulnerabilities
+								97639, #Mozilla Firefox < 52.0 Multiple Vulnerabilities
+								99125, #Mozilla Firefox < 52.0.1 CreateImageBitmap RCE
+								99632, #Mozilla Firefox < 53 Multiple Vulnerabilities
+								100127, #Mozilla Firefox < 53.0.2 ANGLE Graphics Library RCE
+								55901, #Firefox 3.6 < 3.6.20 Multiple Vulnerabilities
+								56334, #Firefox 3.6.x < 3.6.23 Multiple Vulnerabilities
+								56750, #Firefox 3.6.x < 3.6.24 Multiple Vulnerabilities
+								57768, #Firefox < 10.0 Multiple Vulnerabilities
+								57769, #Firefox 3.6.x < 3.6.26 Multiple Vulnerabilities
+								58006, #Firefox 3.6.x < 3.6.27 'png_decompress_chunk' Integer Overflow
+								58349, #Firefox 3.6.x < 3.6.28 Multiple Vulnerabilities
+								58898, #Firefox < 12.0 Multiple Vulnerabilities
+								59407, #Firefox < 13.0 Multiple Vulnerabilities
+								60043, #Firefox < 14.0 Multiple Vulnerabilities
+								61715, #Firefox < 15.0 Multiple Vulnerabilities
+								62580, #Firefox < 16.0 Multiple Vulnerabilities
+								62589, #Firefox < 16.0.1 Multiple Vulnerabilities
+								94232, #Mozilla Firefox 48.x / 49.x < 49.0.2 Multiple Vulnerabilities
+								102359, #Mozilla Firefox < 55 Multiple Vulnerabilities
+								100810, #Mozilla Firefox < 54 Multiple Vulnerabilities
+								103680, #Mozilla Firefox < 56 Multiple Vulnerabilities
 							]
 						}
 					end
