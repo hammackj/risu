160aab22d6745f95d4006cc197949516f90175e2
Jacob Hammack <jacob.hammack@hammackj.com>
2019-06-05 20:06:03 -0500

    Added new postprocessing

---
+++ lib/risu/parsers/nessus/postprocess/mongo_db.rb
@@ -0,0 +1,46 @@
+# Copyright (c) 2010-2019 Jacob Hammack.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+module Risu
+	module Parsers
+		module Nessus
+			module PostProcess
+				class MongoDBRollup < Risu::Base::PostProcessBase
+
+					#
+					def initialize
+						@info =
+						{
+							:description => "MongoDB Patch Rollup",
+							:plugin_id => -99943,
+							:plugin_name => "Missing the latest MongoDB",
+							:item_name => "Update to the latest MongoDB",
+							:plugin_ids => [
+122243, #MongoDB 2.6.x < 2.6.9, 3.0.x < 3.0.14, 3.2.x < 3.2.8 mongod
+
+							]
+						}
+					end
+				end
+			end
+		end
+	end
+end
